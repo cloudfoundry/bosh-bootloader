df4b19dd934b6902f76b33400b7da5e000137b7d
George Dean <gdean@pivotal.io>
2016-01-29 16:38:33 -0800

    Use gexec to invoke `bbl` in test runs
    [#108058534]
    
    Signed-off-by: Ryan Moran <rmoran@pivotal.io>

--- bbl/bbl_test.go
+++ bbl/bbl_test.go
@@ -1,44 +1,39 @@
 package main_test
 
 import (
-	"bytes"
 	"os/exec"
 
+	"github.com/gomega/gexec"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-const COMMAND_TIMEOUT = "1s"
-
 var _ = Describe("bbl", func() {
 	Describe("bbl -h", func() {
 		It("prints out the usage", func() {
-			command := exec.Command(pathToBBL, "-h")
-			output := bytes.NewBuffer([]byte{})
-			command.Stdout = output
+			session, err := gexec.Start(exec.Command(pathToBBL, "-h"), GinkgoWriter, GinkgoWriter)
 
-			Eventually(command.Run, COMMAND_TIMEOUT, COMMAND_TIMEOUT).Should(Succeed())
-			Expect(output).To(ContainSubstring("Usage"))
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+			Expect(session.Out.Contents()).To(ContainSubstring("usage"))
 		})
 	})
 
 	Describe("bbl -v", func() {
 		It("prints out the current version", func() {
-			command := exec.Command(pathToBBL, "-v")
-			output := bytes.NewBuffer([]byte{})
-			command.Stdout = output
+			session, err := gexec.Start(exec.Command(pathToBBL, "-v"), GinkgoWriter, GinkgoWriter)
 
-			Eventually(command.Run, COMMAND_TIMEOUT, COMMAND_TIMEOUT).Should(Succeed())
-			Expect(output).To(ContainSubstring("bbl 0.0.1"))
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit(0))
+			Expect(session.Out.Contents()).To(ContainSubstring("bbl 0.0.1"))
 		})
 	})
 
 	It("prints an error when an unknown flag is provided", func() {
-		command := exec.Command(pathToBBL, "--some-unknown-flag")
-		errors := bytes.NewBuffer([]byte{})
-		command.Stderr = errors
+		session, err := gexec.Start(exec.Command(pathToBBL, "--some-unknown-flag"), GinkgoWriter, GinkgoWriter)
 
-		Eventually(command.Run, COMMAND_TIMEOUT, COMMAND_TIMEOUT).ShouldNot(Succeed())
-		Expect(errors).To(ContainSubstring("unknown flag `some-unknown-flag'"))
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(session).Should(gexec.Exit(1))
+		Expect(session.Err.Contents()).To(ContainSubstring("unknown flag `some-unknown-flag'"))
 	})
 })
