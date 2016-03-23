f590ad1297da69ba21a39e978fd2da2d36c08f94
George Dean <gdean@pivotal.io>
2016-03-23 16:12:48 -0700

    Delete unused issue.go
    [#115175733]
    
    Signed-off-by: Jay Dunkelberger <ldunkelberger@pivotal.io>

--- commands/unsupported/issue.go
+++
@@ -1,7 +0,0 @@
-package unsupported
-
-import "fmt"
-
-func NewIssue(message string) error {
-	return fmt.Errorf("%s, please open an issue at https://github.com/pivotal-cf-experimental/bosh-bootloader/issues/new if you require assistance.", message)
-}
