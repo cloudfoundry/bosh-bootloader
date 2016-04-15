a5805b1343c8de4860b5c6ddf002709210ee6ecc
Christian Ang <cang@pivotal.io>
2016-04-15 09:50:37 -0700

    Refactored LoadBalancer Subnet
    - Improved readability of function
    
    [#115989445]
    
    Signed-off-by: Kevin Kelani <kkelani@pivotal.io>

--- aws/cloudformation/templates/load_balancer_subnet_template_builder.go
+++ aws/cloudformation/templates/load_balancer_subnet_template_builder.go
@@ -9,32 +9,38 @@ func NewLoadBalancerSubnetTemplateBuilder() LoadBalancerSubnetTemplateBuilder {
 }
 
 func (LoadBalancerSubnetTemplateBuilder) LoadBalancerSubnet(azIndex int, subnetSuffix string, cidrBlock string) Template {
+	subnetName := fmt.Sprintf("LoadBalancerSubnet%s", subnetSuffix)
+	cidrName := fmt.Sprintf("%sCIDR", subnetName)
+	az := fmt.Sprintf("%d", azIndex)
+	tag := fmt.Sprintf("LoadBalancer%s", subnetSuffix)
+	routeTableAssociationName := fmt.Sprintf("%sRouteTableAssociation", subnetName)
+
 	return Template{
 		Parameters: map[string]Parameter{
-			fmt.Sprintf("LoadBalancerSubnet%sCIDR", subnetSuffix): Parameter{
+			cidrName: Parameter{
 				Description: "CIDR block for the ELB subnet.",
 				Type:        "String",
 				Default:     cidrBlock,
 			},
 		},
 		Resources: map[string]Resource{
-			fmt.Sprintf("LoadBalancerSubnet%s", subnetSuffix): Resource{
+			subnetName: Resource{
 				Type: "AWS::EC2::Subnet",
 				Properties: Subnet{
 					AvailabilityZone: map[string]interface{}{
 						"Fn::Select": []interface{}{
-							fmt.Sprintf("%d", azIndex),
+							az,
 							map[string]Ref{
 								"Fn::GetAZs": Ref{"AWS::Region"},
 							},
 						},
 					},
-					CidrBlock: Ref{fmt.Sprintf("LoadBalancerSubnet%sCIDR", subnetSuffix)},
+					CidrBlock: Ref{cidrName},
 					VpcId:     Ref{"VPC"},
 					Tags: []Tag{
 						{
 							Key:   "Name",
-							Value: fmt.Sprintf("LoadBalancer%s", subnetSuffix),
+							Value: tag,
 						},
 					},
 				},
@@ -54,11 +60,11 @@ func (LoadBalancerSubnetTemplateBuilder) LoadBalancerSubnet(azIndex int, subnetS
 					RouteTableId:         Ref{"LoadBalancerRouteTable"},
 				},
 			},
-			fmt.Sprintf("LoadBalancerSubnet%sRouteTableAssociation", subnetSuffix): {
+			routeTableAssociationName: {
 				Type: "AWS::EC2::SubnetRouteTableAssociation",
 				Properties: SubnetRouteTableAssociation{
 					RouteTableId: Ref{"LoadBalancerRouteTable"},
-					SubnetId:     Ref{fmt.Sprintf("LoadBalancerSubnet%s", subnetSuffix)},
+					SubnetId:     Ref{subnetName},
 				},
 			},
 		},
