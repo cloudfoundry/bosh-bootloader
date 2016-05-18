ae8a08366a0072254f7f62c08c0871d653b91610
Christian Ang <cang@pivotal.io>
2016-05-18 15:01:15 -0700

    Remove unused aws.Config.ValidateCredentials
    [#119210765]
    
    Signed-off-by: George Dean <gdean@pivotal.io>

--- aws/config.go
+++ aws/config.go
@@ -1,8 +1,6 @@
 package aws
 
 import (
-	"errors"
-
 	goaws "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 )
@@ -12,22 +10,6 @@ type Config struct {
 	SecretAccessKey  string
 	Region           string
 	EndpointOverride string
-}
-
-func (c Config) ValidateCredentials() error {
-	if c.AccessKeyID == "" {
-		return errors.New("--aws-access-key-id must be provided")
-	}
-
-	if c.SecretAccessKey == "" {
-		return errors.New("--aws-secret-access-key must be provided")
-	}
-
-	if c.Region == "" {
-		return errors.New("--aws-region must be provided")
-	}
-
-	return nil
 }
 
 func (c Config) ClientConfig() *goaws.Config {
--- aws/config_test.go
+++ aws/config_test.go
@@ -10,48 +10,7 @@ import (
 )
 
 var _ = Describe("Config", func() {
-	Context("ValidateCredentials", func() {
-		It("validates that the credentials have been set", func() {
-			config := aws.Config{
-				AccessKeyID:     "some-access-key-id",
-				SecretAccessKey: " some-secret-access-key",
-				Region:          "some-region",
-			}
-
-			err := config.ValidateCredentials()
-			Expect(err).NotTo(HaveOccurred())
-		})
-		Context("failure cases", func() {
-			It("returns an error when the access key id is missing", func() {
-				config := aws.Config{
-					SecretAccessKey: "some-secret-access-key",
-					Region:          "some-region",
-				}
-
-				Expect(config.ValidateCredentials()).To(MatchError("--aws-access-key-id must be provided"))
-			})
-
-			It("returns an error when the secret access key is missing", func() {
-				config := aws.Config{
-					AccessKeyID: "some-access-key-id",
-					Region:      "some-region",
-				}
-
-				Expect(config.ValidateCredentials()).To(MatchError("--aws-secret-access-key must be provided"))
-			})
-
-			It("returns an error when the region is missing", func() {
-				config := aws.Config{
-					AccessKeyID:     "some-access-key-id",
-					SecretAccessKey: "some-secret-access-key",
-				}
-
-				Expect(config.ValidateCredentials()).To(MatchError("--aws-region must be provided"))
-			})
-		})
-	})
-
-	Context("ClientConfig", func() {
+	Describe("ClientConfig", func() {
 		It("returns an AWS config which is consumable by AWS client functions", func() {
 			config := aws.Config{
 				AccessKeyID:      "some-access-key-id",
