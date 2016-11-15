a48f6e1981083bedd15e0c8ab37a71670c71f41d
Kevin Kelani <kkelani@pivotal.io>
2016-11-15 10:34:23 -0800

    Clean up from previous commit refactor
    [#133063377]
    
    Signed-off-by: Angela Chin <achin@pivotal.io>

--- fakes/gcp_project_accessor.go
+++
@@ -1,7 +0,0 @@
-package fakes
-
-import "google.golang.org/api/compute/v1"
-
-type GCPProjectAccessor interface {
-	SetCommonInstanceMetadata(project string, metadata *compute.Metadata) *compute.ProjectsSetCommonInstanceMetadataCall
-}
--- gcp/client.go
+++ gcp/client.go
@@ -6,3 +6,15 @@ type Client interface {
 	GetProject(projectID string) (*compute.Project, error)
 	SetCommonInstanceMetadata(projectID string, metadata *compute.Metadata) (*compute.Operation, error)
 }
+
+type GCPClient struct {
+	service *compute.Service
+}
+
+func (c GCPClient) GetProject(projectID string) (*compute.Project, error) {
+	return c.service.Projects.Get(projectID).Do()
+}
+
+func (c GCPClient) SetCommonInstanceMetadata(projectID string, metadata *compute.Metadata) (*compute.Operation, error) {
+	return c.service.Projects.SetCommonInstanceMetadata(projectID, metadata).Do()
+}
