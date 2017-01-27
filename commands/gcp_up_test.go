9a214f3fb7140a1b127241014bdf22be4c5e78f3
Christian Ang <cang@pivotal.io>
2017-01-26 16:49:28 -0800

    gcp up is idempotent with lbs
    [#137994865]
    
    Signed-off-by: Nick Mahoney <nmahoney@pivotal.io>

--- commands/gcp_up_test.go
+++ commands/gcp_up_test.go
@@ -349,7 +349,7 @@ var _ = Describe("gcp up", func() {
 			})
 
 			Context("failure cases", func() {
-				DescribeTable("returns an error when we fail to get an output", func(outputName string) {
+				DescribeTable("returns an error when we fail to get an output", func(outputName, lb string) {
 					terraformOutputter.GetCall.Stub = func(output string) (string, error) {
 						if output == outputName {
 							return "", errors.New("failed to get output")
@@ -362,15 +362,24 @@ var _ = Describe("gcp up", func() {
 						ProjectID:             "some-project-id",
 						Zone:                  "some-zone",
 						Region:                "us-west1",
-					}, storage.State{})
+					}, storage.State{
+						LB: storage.LB{
+							Type: lb,
+						},
+					})
 					Expect(err).To(MatchError("failed to get output"))
 				},
-					Entry("failed to get external_ip", "external_ip"),
-					Entry("failed to get network_name", "network_name"),
-					Entry("failed to get subnetwork_name", "subnetwork_name"),
-					Entry("failed to get bosh_open_tag_name", "bosh_open_tag_name"),
-					Entry("failed to get internal_tag_name", "internal_tag_name"),
-					Entry("failed to get director_address", "director_address"),
+					Entry("failed to get external_ip", "external_ip", ""),
+					Entry("failed to get network_name", "network_name", ""),
+					Entry("failed to get subnetwork_name", "subnetwork_name", ""),
+					Entry("failed to get bosh_open_tag_name", "bosh_open_tag_name", ""),
+					Entry("failed to get internal_tag_name", "internal_tag_name", ""),
+					Entry("failed to get director_address", "director_address", ""),
+					Entry("failed to get router_backend_service", "router_backend_service", "cf"),
+					Entry("failed to get ssh_proxy_target_pool", "ssh_proxy_target_pool", "cf"),
+					Entry("failed to get tcp_router_target_pool", "tcp_router_target_pool", "cf"),
+					Entry("failed to get ws_target_pool", "ws_target_pool", "cf"),
+					Entry("failed to get concourse_target_pool", "concourse_target_pool", "concourse"),
 				)
 
 				It("returns an error if applier fails with non terraform apply error", func() {
@@ -463,6 +472,88 @@ var _ = Describe("gcp up", func() {
 			}))
 
 			Expect(boshClient.UpdateCloudConfigCall.CallCount).To(Equal(1))
+		})
+
+		Context("when a cf lb exists", func() {
+			BeforeEach(func() {
+				terraformOutputter.GetCall.Stub = func(output string) (string, error) {
+					switch output {
+					case "router_backend_service":
+						return "env-id-cf-https-lb", nil
+					case "ws_target_pool":
+						return "env-id-cf-ws", nil
+					case "ssh_proxy_target_pool":
+						return "env-id-cf-ssh-proxy-lb", nil
+					case "tcp_router_target_pool":
+						return "env-id-cf-tcp-router-network-properties", nil
+					case "concourse_target_pool":
+						return "", errors.New("output does not exist")
+					default:
+						return "", nil
+					}
+				}
+			})
+
+			It("generates a cloud config with cf lb information", func() {
+				err := gcpUp.Execute(commands.GCPUpConfig{
+					ServiceAccountKeyPath: serviceAccountKeyPath,
+					ProjectID:             "some-project-id",
+					Zone:                  "some-zone",
+					Region:                "some-region",
+				}, storage.State{
+					EnvID: "bbl-lake-time:stamp",
+					LB: storage.LB{
+						Type:   "cf",
+						Cert:   "some-cert",
+						Key:    "some-key",
+						Domain: "some-domain",
+					},
+				})
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(gcpCloudConfigGenerator.GenerateCall.Receives.CloudConfigInput.CFBackends.Router).To(Equal("env-id-cf-https-lb"))
+				Expect(gcpCloudConfigGenerator.GenerateCall.Receives.CloudConfigInput.CFBackends.SSHProxy).To(Equal("env-id-cf-ssh-proxy-lb"))
+				Expect(gcpCloudConfigGenerator.GenerateCall.Receives.CloudConfigInput.CFBackends.TCPRouter).To(Equal("env-id-cf-tcp-router-network-properties"))
+				Expect(gcpCloudConfigGenerator.GenerateCall.Receives.CloudConfigInput.CFBackends.WS).To(Equal("env-id-cf-ws"))
+			})
+		})
+
+		Context("when a concourse lb exists", func() {
+			BeforeEach(func() {
+				terraformOutputter.GetCall.Stub = func(output string) (string, error) {
+					switch output {
+					case "concourse_target_pool":
+						return "env-id-concourse-target-pool", nil
+					case "router_backend_service":
+						return "", errors.New("output does not exist")
+					case "ws_target_pool":
+						return "", errors.New("output does not exist")
+					case "ssh_proxy_target_pool":
+						return "", errors.New("output does not exist")
+					case "tcp_router_target_pool":
+						return "", errors.New("output does not exist")
+					default:
+						return "", nil
+					}
+				}
+			})
+
+			It("generates a cloud config with concourse lb information", func() {
+				err := gcpUp.Execute(commands.GCPUpConfig{
+					ServiceAccountKeyPath: serviceAccountKeyPath,
+					ProjectID:             "some-project-id",
+					Zone:                  "some-zone",
+					Region:                "some-region",
+				}, storage.State{
+					EnvID: "bbl-lake-time:stamp",
+					LB: storage.LB{
+						Type: "concourse",
+					},
+				})
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(gcpCloudConfigGenerator.GenerateCall.Receives.CloudConfigInput.ConcourseTargetPool).To(Equal("env-id-concourse-target-pool"))
+			})
 		})
 
 		Context("failure cases", func() {
