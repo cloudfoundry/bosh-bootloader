dccff390aacbff344dcd4dbb12c631fa58bb593f
Genevieve LEsperance <glesperance@pivotal.io>
2017-05-23 13:54:27 -0700

    Removing unused gcp terraform templates.

--- terraform/gcp/gcp_templates.go
+++ terraform/gcp/gcp_templates.go
@@ -103,369 +103,6 @@ resource "google_compute_firewall" "internal" {
 }
 `
 
-const terraformConcourseLBTemplate = `output "concourse_target_pool" {
-	value = "${google_compute_target_pool.target-pool.name}"
-}
-
-output "concourse_lb_ip" {
-    value = "${google_compute_address.concourse-address.address}"
-}
-
-resource "google_compute_firewall" "firewall-concourse" {
-  name    = "${var.env_id}-concourse-open"
-  network = "${google_compute_network.bbl-network.name}"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["443", "2222"]
-  }
-
-  target_tags = ["concourse"]
-}
-
-resource "google_compute_address" "concourse-address" {
-  name = "${var.env_id}-concourse"
-}
-
-resource "google_compute_target_pool" "target-pool" {
-  name = "${var.env_id}-concourse"
-
-  session_affinity = "NONE"
-}
-
-resource "google_compute_forwarding_rule" "ssh-forwarding-rule" {
-  name        = "${var.env_id}-concourse-ssh"
-  target      = "${google_compute_target_pool.target-pool.self_link}"
-  port_range  = "2222"
-  ip_protocol = "TCP"
-  ip_address  = "${google_compute_address.concourse-address.address}"
-}
-
-resource "google_compute_forwarding_rule" "https-forwarding-rule" {
-  name        = "${var.env_id}-concourse-https"
-  target      = "${google_compute_target_pool.target-pool.self_link}"
-  port_range  = "443"
-  ip_protocol = "TCP"
-  ip_address  = "${google_compute_address.concourse-address.address}"
-}
-`
-
-const terraformCFLBTemplate = `variable "ssl_certificate" {
-  type = "string"
-}
-
-variable "ssl_certificate_private_key" {
-  type = "string"
-}
-
-output "router_backend_service" {
-  value = "${google_compute_backend_service.router-lb-backend-service.name}"
-}
-
-output "router_lb_ip" {
-    value = "${google_compute_global_address.cf-address.address}"
-}
-
-output "ssh_proxy_lb_ip" {
-    value = "${google_compute_address.cf-ssh-proxy.address}"
-}
-
-output "tcp_router_lb_ip" {
-    value = "${google_compute_address.cf-tcp-router.address}"
-}
-
-output "ws_lb_ip" {
-    value = "${google_compute_address.cf-ws.address}"
-}
-
-resource "google_compute_firewall" "firewall-cf" {
-  name       = "${var.env_id}-cf-open"
-  depends_on = ["google_compute_network.bbl-network"]
-  network    = "${google_compute_network.bbl-network.name}"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "443"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-
-  target_tags = ["${google_compute_backend_service.router-lb-backend-service.name}"]
-}
-
-resource "google_compute_global_address" "cf-address" {
-  name = "${var.env_id}-cf"
-}
-
-resource "google_compute_global_forwarding_rule" "cf-http-forwarding-rule" {
-  name       = "${var.env_id}-cf-http"
-  ip_address = "${google_compute_global_address.cf-address.address}"
-  target     = "${google_compute_target_http_proxy.cf-http-lb-proxy.self_link}"
-  port_range = "80"
-}
-
-resource "google_compute_global_forwarding_rule" "cf-https-forwarding-rule" {
-  name       = "${var.env_id}-cf-https"
-  ip_address = "${google_compute_global_address.cf-address.address}"
-  target     = "${google_compute_target_https_proxy.cf-https-lb-proxy.self_link}"
-  port_range = "443"
-}
-
-resource "google_compute_target_http_proxy" "cf-http-lb-proxy" {
-  name        = "${var.env_id}-http-proxy"
-  description = "really a load balancer but listed as an http proxy"
-  url_map     = "${google_compute_url_map.cf-https-lb-url-map.self_link}"
-}
-
-resource "google_compute_target_https_proxy" "cf-https-lb-proxy" {
-  name             = "${var.env_id}-https-proxy"
-  description      = "really a load balancer but listed as an https proxy"
-  url_map          = "${google_compute_url_map.cf-https-lb-url-map.self_link}"
-  ssl_certificates = ["${google_compute_ssl_certificate.cf-cert.self_link}"]
-}
-
-resource "google_compute_ssl_certificate" "cf-cert" {
-  name_prefix = "${var.env_id}"
-  description = "user provided ssl private key / ssl certificate pair"
-  private_key = "${file(var.ssl_certificate_private_key)}"
-  certificate = "${file(var.ssl_certificate)}"
-  lifecycle {
-	create_before_destroy = true
-  }
-}
-
-resource "google_compute_url_map" "cf-https-lb-url-map" {
-  name = "${var.env_id}-cf-http"
-
-  default_service = "${google_compute_backend_service.router-lb-backend-service.self_link}"
-}
-
-resource "google_compute_http_health_check" "cf-public-health-check" {
-  name                = "${var.env_id}-cf"
-  port                = 8080
-  request_path        = "/health"
-}
-
-resource "google_compute_firewall" "cf-health-check" {
-  name       = "${var.env_id}-cf-health-check"
-  depends_on = ["google_compute_network.bbl-network"]
-  network    = "${google_compute_network.bbl-network.name}"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["8080", "80"]
-  }
-
-  source_ranges = ["130.211.0.0/22"]
-  target_tags   = ["${google_compute_backend_service.router-lb-backend-service.name}"]
-}
-
-output "ssh_proxy_target_pool" {
-  value = "${google_compute_target_pool.cf-ssh-proxy.name}"
-}
-
-resource "google_compute_address" "cf-ssh-proxy" {
-  name = "${var.env_id}-cf-ssh-proxy"
-}
-
-resource "google_compute_firewall" "cf-ssh-proxy" {
-  name       = "${var.env_id}-cf-ssh-proxy-open"
-  depends_on = ["google_compute_network.bbl-network"]
-  network    = "${google_compute_network.bbl-network.name}"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["2222"]
-  }
-
-  target_tags = ["${google_compute_target_pool.cf-ssh-proxy.name}"]
-}
-
-resource "google_compute_target_pool" "cf-ssh-proxy" {
-  name = "${var.env_id}-cf-ssh-proxy"
-
-  session_affinity = "NONE"
-}
-
-resource "google_compute_forwarding_rule" "cf-ssh-proxy" {
-  name        = "${var.env_id}-cf-ssh-proxy"
-  target      = "${google_compute_target_pool.cf-ssh-proxy.self_link}"
-  port_range  = "2222"
-  ip_protocol = "TCP"
-  ip_address  = "${google_compute_address.cf-ssh-proxy.address}"
-}
-
-output "tcp_router_target_pool" {
-  value = "${google_compute_target_pool.cf-tcp-router.name}"
-}
-
-resource "google_compute_firewall" "cf-tcp-router" {
-  name       = "${var.env_id}-cf-tcp-router"
-  depends_on = ["google_compute_network.bbl-network"]
-  network    = "${google_compute_network.bbl-network.name}"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["1024-32768"]
-  }
-
-  target_tags = ["${google_compute_target_pool.cf-tcp-router.name}"]
-}
-
-resource "google_compute_address" "cf-tcp-router" {
-  name = "${var.env_id}-cf-tcp-router"
-}
-
-resource "google_compute_http_health_check" "cf-tcp-router" {
-  name                = "${var.env_id}-cf-tcp-router"
-  port                = 80
-  request_path        = "/health"
-}
-
-resource "google_compute_target_pool" "cf-tcp-router" {
-  name = "${var.env_id}-cf-tcp-router"
-
-  session_affinity = "NONE"
-
-  health_checks = [
-    "${google_compute_http_health_check.cf-tcp-router.name}",
-  ]
-}
-
-resource "google_compute_forwarding_rule" "cf-tcp-router" {
-  name        = "${var.env_id}-cf-tcp-router"
-  target      = "${google_compute_target_pool.cf-tcp-router.self_link}"
-  port_range  = "1024-32768"
-  ip_protocol = "TCP"
-  ip_address  = "${google_compute_address.cf-tcp-router.address}"
-}
-
-output "ws_target_pool" {
-  value = "${google_compute_target_pool.cf-ws.name}"
-}
-
-resource "google_compute_address" "cf-ws" {
-  name = "${var.env_id}-cf-ws"
-}
-
-resource "google_compute_target_pool" "cf-ws" {
-  name = "${var.env_id}-cf-ws"
-
-  session_affinity = "NONE"
-
-  health_checks = ["${google_compute_http_health_check.cf-public-health-check.name}"]
-}
-
-resource "google_compute_forwarding_rule" "cf-ws-https" {
-  name        = "${var.env_id}-cf-ws-https"
-  target      = "${google_compute_target_pool.cf-ws.self_link}"
-  port_range  = "443"
-  ip_protocol = "TCP"
-  ip_address  = "${google_compute_address.cf-ws.address}"
-}
-
-resource "google_compute_forwarding_rule" "cf-ws-http" {
-  name        = "${var.env_id}-cf-ws-http"
-  target      = "${google_compute_target_pool.cf-ws.self_link}"
-  port_range  = "80"
-  ip_protocol = "TCP"
-  ip_address  = "${google_compute_address.cf-ws.address}"
-}
-`
-
-const terraformCFDNSTemplate = `
-variable "system_domain" {
-  type = "string"
-}
-
-resource "google_dns_managed_zone" "env_dns_zone" {
-  name        = "${var.env_id}-zone"
-  dns_name    = "${var.system_domain}."
-  description = "DNS zone for the ${var.env_id} environment"
-}
-
-output "system_domain_dns_servers" {
-  value = "${google_dns_managed_zone.env_dns_zone.name_servers}"
-}
-
-resource "google_dns_record_set" "wildcard-dns" {
-  name       = "*.${google_dns_managed_zone.env_dns_zone.dns_name}"
-  depends_on = ["google_compute_global_address.cf-address"]
-  type       = "A"
-  ttl        = 300
-
-  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
-
-  rrdatas = ["${google_compute_global_address.cf-address.address}"]
-}
-
-resource "google_dns_record_set" "bosh-dns" {
-  name       = "bosh.${google_dns_managed_zone.env_dns_zone.dns_name}"
-  depends_on = ["google_compute_address.bosh-external-ip"]
-  type       = "A"
-  ttl        = 300
-
-  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
-
-  rrdatas = ["${google_compute_address.bosh-external-ip.address}"]
-}
-
-resource "google_dns_record_set" "cf-ssh-proxy" {
-  name       = "ssh.${google_dns_managed_zone.env_dns_zone.dns_name}"
-  depends_on = ["google_compute_address.cf-ssh-proxy"]
-  type       = "A"
-  ttl        = 300
-
-  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
-
-  rrdatas = ["${google_compute_address.cf-ssh-proxy.address}"]
-}
-
-resource "google_dns_record_set" "tcp-dns" {
-  name       = "tcp.${google_dns_managed_zone.env_dns_zone.dns_name}"
-  depends_on = ["google_compute_address.cf-tcp-router"]
-  type       = "A"
-  ttl        = 300
-
-  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
-
-  rrdatas = ["${google_compute_address.cf-tcp-router.address}"]
-}
-
-resource "google_dns_record_set" "doppler-dns" {
-  name       = "doppler.${google_dns_managed_zone.env_dns_zone.dns_name}"
-  depends_on = ["google_compute_address.cf-ws"]
-  type       = "A"
-  ttl        = 300
-
-  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
-
-  rrdatas = ["${google_compute_address.cf-ws.address}"]
-}
-
-resource "google_dns_record_set" "loggregator-dns" {
-  name       = "loggregator.${google_dns_managed_zone.env_dns_zone.dns_name}"
-  depends_on = ["google_compute_address.cf-ws"]
-  type       = "A"
-  ttl        = 300
-
-  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
-
-  rrdatas = ["${google_compute_address.cf-ws.address}"]
-}
-
-resource "google_dns_record_set" "wildcard-ws-dns" {
-  name       = "*.ws.${google_dns_managed_zone.env_dns_zone.dns_name}"
-  depends_on = ["google_compute_address.cf-ws"]
-  type       = "A"
-  ttl        = 300
-
-  managed_zone = "${google_dns_managed_zone.env_dns_zone.name}"
-
-  rrdatas = ["${google_compute_address.cf-ws.address}"]
-}
-`
-
 const ConcourseLBTemplate = `output "concourse_target_pool" {
 	value = "${google_compute_target_pool.target-pool.name}"
 }
@@ -492,6 +129,8 @@ resource "google_compute_address" "concourse-address" {
 
 resource "google_compute_target_pool" "target-pool" {
   name = "${var.env_id}-concourse"
+
+  session_affinity = "NONE"
 }
 
 resource "google_compute_forwarding_rule" "ssh-forwarding-rule" {
@@ -644,6 +283,8 @@ resource "google_compute_firewall" "cf-ssh-proxy" {
 
 resource "google_compute_target_pool" "cf-ssh-proxy" {
   name = "${var.env_id}-cf-ssh-proxy"
+
+  session_affinity = "NONE"
 }
 
 resource "google_compute_forwarding_rule" "cf-ssh-proxy" {
@@ -684,6 +325,8 @@ resource "google_compute_http_health_check" "cf-tcp-router" {
 resource "google_compute_target_pool" "cf-tcp-router" {
   name = "${var.env_id}-cf-tcp-router"
 
+  session_affinity = "NONE"
+
   health_checks = [
     "${google_compute_http_health_check.cf-tcp-router.name}",
   ]
@@ -707,6 +350,8 @@ resource "google_compute_address" "cf-ws" {
 
 resource "google_compute_target_pool" "cf-ws" {
   name = "${var.env_id}-cf-ws"
+
+  session_affinity = "NONE"
 
   health_checks = ["${google_compute_http_health_check.cf-public-health-check.name}"]
 }
