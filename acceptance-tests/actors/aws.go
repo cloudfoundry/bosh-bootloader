4adfb9b1ea9a9f390f885aff225f5f7e0baa0a78
Genevieve LEsperance <glesperance@pivotal.io>
2017-07-20 11:46:38 -0700

    Add back check if cert arn was split successfully.

--- acceptance-tests/actors/aws.go
+++ acceptance-tests/actors/aws.go
@@ -152,6 +152,7 @@ func (a AWS) GetSSLCertificateNameFromLBs(envID string) string {
 				if int(*ld.Listener.LoadBalancerPort) == 443 {
 					certificateArn := ld.Listener.SSLCertificateId
 					certificateArnParts := strings.Split(awslib.StringValue(certificateArn), "/")
+					Expect(certificateArnParts).To(HaveLen(2))
 					certificateName = certificateArnParts[1]
 					Expect(certificateName).NotTo(BeEmpty())
 
