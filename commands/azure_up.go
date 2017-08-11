938f9d8ee8080577d06ab35c1e374b75888554af
Nicolas Roy <niroy@microsoft.com>
2017-08-11 11:39:27 -0700

    Azure up syncs the env-id

--- commands/azure_up.go
+++ commands/azure_up.go
@@ -2,7 +2,6 @@ package commands
 
 import (
 	"errors"
-
 	"github.com/cloudfoundry/bosh-bootloader/storage"
 )
 
@@ -10,25 +9,42 @@ type azureClient interface {
 	ValidateCredentials(subscriptionID, tenantID, clientID, clientSecret string) error
 }
 
-type AzureUpConfig struct{}
-
-type AzureUp struct {
-	azureClient azureClient
-	logger      logger
+type AzureUpConfig struct {
+	Name string
 }
 
-func NewAzureUp(azureClient azureClient, logger logger) AzureUp {
+type AzureUp struct {
+	azureClient  azureClient
+	logger       logger
+	envIDManager envIDManager
+	stateStore   stateStore
+}
+
+func NewAzureUp(azureClient azureClient, logger logger, envIDManager envIDManager, stateStore stateStore) AzureUp {
 	return AzureUp{
-		azureClient: azureClient,
-		logger:      logger,
+		azureClient:  azureClient,
+		logger:       logger,
+		envIDManager: envIDManager,
+		stateStore:   stateStore,
 	}
 }
 
 func (u AzureUp) Execute(upConfig AzureUpConfig, state storage.State) error {
 	u.logger.Step("verifying credentials")
 	err := u.azureClient.ValidateCredentials(state.Azure.SubscriptionID, state.Azure.TenantID, state.Azure.ClientID, state.Azure.ClientSecret)
+
 	if err != nil {
 		return errors.New("Error: credentials are invalid")
 	}
+
+	state, err = u.envIDManager.Sync(state, upConfig.Name)
+	if err != nil {
+		return err
+	}
+
+	if err := u.stateStore.Set(state); err != nil {
+		return err
+	}
+
 	return nil
 }
