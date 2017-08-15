516a4319b31f0d8e1f89841afa7e55c7fc832c23
Genevieve LEsperance <glesperance@pivotal.io>
2017-08-15 15:42:50 -0700

    Add test coverage to azure input generator.
    [#150277362]

---
+++ terraform/azure/input_generator_test.go
@@ -0,0 +1,44 @@
+package azure_test
+
+import (
+	"github.com/cloudfoundry/bosh-bootloader/storage"
+	"github.com/cloudfoundry/bosh-bootloader/terraform/azure"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("InputGenerator", func() {
+	var (
+		inputGenerator azure.InputGenerator
+
+		state storage.State
+	)
+
+	BeforeEach(func() {
+		state = storage.State{
+			IAAS:  "azure",
+			EnvID: "env-id",
+			Azure: storage.Azure{
+				SubscriptionID: "subscription-id",
+				TenantID:       "tenant-id",
+				ClientID:       "client-id",
+				ClientSecret:   "client-secret",
+			},
+		}
+
+		inputGenerator = azure.NewInputGenerator()
+	})
+
+	It("receives BBL state and returns a map of terraform variables", func() {
+		inputs, err := inputGenerator.Generate(state)
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(inputs).To(Equal(map[string]string{
+			"env_id":          state.EnvID,
+			"subscription_id": state.Azure.SubscriptionID,
+			"tenant_id":       state.Azure.TenantID,
+			"client_id":       state.Azure.ClientID,
+			"client_secret":   state.Azure.ClientSecret,
+		}))
+	})
+})
