d840f6d6c76279f3bd745499dc7b41d2f7f8f940
Genevieve LEsperance <glesperance@pivotal.io>
2017-08-18 10:07:54 -0700

    Add azure output generator test.

--- terraform/azure/output_generator_test.go
+++ terraform/azure/output_generator_test.go
@@ -1,1 +1,45 @@
 package azure_test
+
+import (
+	"errors"
+
+	"github.com/cloudfoundry/bosh-bootloader/fakes"
+	"github.com/cloudfoundry/bosh-bootloader/terraform/azure"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("OutputGenerator", func() {
+	Describe("Generate", func() {
+		var (
+			executor        *fakes.TerraformExecutor
+			outputGenerator azure.OutputGenerator
+		)
+
+		BeforeEach(func() {
+			executor = &fakes.TerraformExecutor{}
+			outputGenerator = azure.NewOutputGenerator(executor)
+
+			executor.OutputsCall.Returns.Outputs = map[string]interface{}{
+				"some-key": "some-value",
+			}
+		})
+
+		It("returns the outputs from the terraform state", func() {
+			outputs, err := outputGenerator.Generate("some-key: some-value")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(outputs).To(HaveKeyWithValue("some-key", "some-value"))
+		})
+
+		Context("when executor outputs returns an error", func() {
+			It("returns an empty map and the error", func() {
+				executor.OutputsCall.Returns.Error = errors.New("executor outputs failed")
+
+				outputs, err := outputGenerator.Generate("")
+				Expect(err).To(MatchError("executor outputs failed"))
+				Expect(outputs).To(BeEmpty())
+			})
+		})
+	})
+})
