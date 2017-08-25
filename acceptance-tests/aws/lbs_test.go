9da8ac5dc9082c6d005d189fd3c5b8540d14309a
Genevieve LEsperance <glesperance@pivotal.io>
2017-08-25 16:42:49 -0700

    Remove concourse-lbs coverage from aws lbs acceptance test.
    - This is covered in the concourse-acceptance-tests job run against aws
    in CI.
    
    Signed-off-by: Dave McClure <dmcclure@pivotal.io>

--- acceptance-tests/aws/lbs_test.go
+++ acceptance-tests/aws/lbs_test.go
@@ -16,11 +16,9 @@ import (
 
 var _ = Describe("lbs test", func() {
 	var (
-		bbl     actors.BBL
-		aws     actors.AWS
-		bosh    actors.BOSH
-		boshcli actors.BOSHCLI
-		state   acceptance.State
+		bbl   actors.BBL
+		aws   actors.AWS
+		state acceptance.State
 
 		certPath       string
 		chainPath      string
@@ -38,8 +36,6 @@ var _ = Describe("lbs test", func() {
 
 		bbl = actors.NewBBL(configuration.StateFileDir, pathToBBL, configuration, "lbs-env")
 		aws = actors.NewAWS(configuration)
-		bosh = actors.NewBOSH()
-		boshcli = actors.NewBOSHCLI()
 		state = acceptance.NewState(configuration.StateFileDir)
 
 		certPath, err = testhelpers.WriteContentsToTempFile(testhelpers.BBL_CERT)
@@ -69,51 +65,6 @@ var _ = Describe("lbs test", func() {
 	AfterEach(func() {
 		session := bbl.Destroy()
 		Eventually(session, 10*time.Minute).Should(gexec.Exit())
-	})
-
-	It("creates, updates and deletes a concourse LB with the specified cert and key", func() {
-		By("verifying there are no load balancers", func() {
-			Expect(aws.LoadBalancers(vpcName)).To(BeEmpty())
-		})
-
-		By("creating a concourse lb", func() {
-			session := bbl.CreateLB("concourse", certPath, keyPath, chainPath)
-			Eventually(session, 10*time.Minute).Should(gexec.Exit(0))
-
-			Expect(aws.LoadBalancers(vpcName)).To(HaveLen(1))
-			Expect(aws.LoadBalancers(vpcName)).To(ConsistOf(MatchRegexp(".*-concourse-lb")))
-
-			certificateName := aws.GetSSLCertificateNameFromLBs(bbl.PredefinedEnvID())
-			Expect(strings.TrimSpace(aws.DescribeCertificate(certificateName).Body)).To(Equal(strings.TrimSpace(testhelpers.BBL_CERT)))
-		})
-
-		By("verifying that the bbl lbs output contains the concourse lb", func() {
-			session := bbl.LBs()
-			Eventually(session).Should(gexec.Exit(0))
-
-			stdout := string(session.Out.Contents())
-			Expect(stdout).To(MatchRegexp("Concourse LB: .*"))
-		})
-
-		By("updating the certs of the lb", func() {
-			session := bbl.UpdateLB(otherCertPath, otherKeyPath, otherChainPath)
-			Eventually(session, 10*time.Minute).Should(gexec.Exit(0))
-
-			Expect(aws.LoadBalancers(vpcName)).To(HaveLen(1))
-			Expect(aws.LoadBalancers(vpcName)).To(ConsistOf(MatchRegexp(".*-concourse-lb")))
-
-			certificateName := aws.GetSSLCertificateNameFromLBs(bbl.PredefinedEnvID())
-			Expect(strings.TrimSpace(aws.DescribeCertificate(certificateName).Body)).To(Equal(strings.TrimSpace(string(testhelpers.OTHER_BBL_CERT))))
-		})
-
-		By("deleting lbs", func() {
-			session := bbl.DeleteLBs()
-			Eventually(session, 15*time.Minute).Should(gexec.Exit(0))
-		})
-
-		By("confirming that the concourse lb does not exist", func() {
-			Expect(aws.LoadBalancers(vpcName)).To(BeEmpty())
-		})
 	})
 
 	It("creates, updates and deletes cf LBs with the specified cert and key", func() {
