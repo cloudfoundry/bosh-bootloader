a30956c34004d359d81d0b03037e35fe7ee2491c
Genevieve LEsperance <glesperance@pivotal.io>
2017-09-08 14:10:22 -0700

    Add director and cloud config check to azure test.
    [#150424004]

--- acceptance-tests/azure/up_test.go
+++ acceptance-tests/azure/up_test.go
@@ -15,10 +15,10 @@ import (
 
 var _ = Describe("up test", func() {
 	var (
-		azure  actors.Azure
-		bbl    actors.BBL
-		config acceptance.Config
-		state  acceptance.State
+		azure   actors.Azure
+		bbl     actors.BBL
+		boshcli actors.BOSHCLI
+		config  acceptance.Config
 	)
 
 	BeforeEach(func() {
@@ -28,12 +28,12 @@ var _ = Describe("up test", func() {
 
 		azure = actors.NewAzure(config)
 		bbl = actors.NewBBL(config.StateFileDir, pathToBBL, config, "azure-env")
-		state = acceptance.NewState(config.StateFileDir)
+		boshcli = actors.NewBOSHCLI()
 	})
 
 	AfterEach(func() {
 		session := bbl.Down()
-		Eventually(session, 10*time.Minute).Should(gexec.Exit(0))
+		Eventually(session, 10*time.Minute).Should(gexec.Exit())
 	})
 
 	It("creates the director", func() {
@@ -43,5 +43,41 @@ var _ = Describe("up test", func() {
 		exists, err := azure.GetResourceGroup(fmt.Sprintf("%s-bosh", bbl.PredefinedEnvID()))
 		Expect(err).NotTo(HaveOccurred())
 		Expect(exists).To(BeTrue())
+
+		directorAddress := bbl.DirectorAddress()
+		caCertPath := bbl.SaveDirectorCA()
+		By("checking if the bosh director exists", func() {
+			exists, err := boshcli.DirectorExists(directorAddress, caCertPath)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(exists).To(BeTrue())
+		})
+
+		By("checking that the cloud config exists", func() {
+			directorUsername := bbl.DirectorUsername()
+			directorPassword := bbl.DirectorPassword()
+
+			cloudConfig, err := boshcli.CloudConfig(directorAddress, caCertPath, directorUsername, directorPassword)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cloudConfig).NotTo(BeEmpty())
+		})
+
+		By("checking if bbl print-env prints the bosh environment variables", func() {
+			stdout := bbl.PrintEnv()
+
+			Expect(stdout).To(ContainSubstring("export BOSH_ENVIRONMENT="))
+			Expect(stdout).To(ContainSubstring("export BOSH_CLIENT="))
+			Expect(stdout).To(ContainSubstring("export BOSH_CLIENT_SECRET="))
+			Expect(stdout).To(ContainSubstring("export BOSH_CA_CERT="))
+		})
+
+		By("checking bbl up with director is idempotent", func() {
+			session := bbl.Up()
+			Eventually(session, 40*time.Minute).Should(gexec.Exit(0))
+		})
+
+		By("destroying the director", func() {
+			session := bbl.Down()
+			Eventually(session, 10*time.Minute).Should(gexec.Exit(0))
+		})
 	})
 })
