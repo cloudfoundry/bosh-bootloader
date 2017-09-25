a8da6b391fa472225274a9a6227777db4a404c51
David McClure <dmcclure@pivotal.io>
2017-09-24 22:12:56 -0700

    Concourse acceptance test works with credhub
    [#151060371]
    
    Signed-off-by: Nick Mahoney <nmahoney@pivotal.io>

--- acceptance-tests/actors/boshcli.go
+++ acceptance-tests/actors/boshcli.go
@@ -53,3 +53,75 @@ func (BOSHCLI) DeleteEnv(stateFilePath, manifestPath string) error {
 
 	return err
 }
+
+func (BOSHCLI) Deploy(address, caCertPath, username, password, deployment, manifest, varsStore string, opsFiles []string, vars map[string]string) error {
+	args := []string{
+		"--ca-cert", caCertPath,
+		"--client", username,
+		"--client-secret", password,
+		"-e", address,
+		"-d", deployment,
+		"deploy", manifest,
+		"--vars-store", varsStore,
+		"-n",
+	}
+	for _, opsFile := range opsFiles {
+		args = append(args, "-o", opsFile)
+	}
+	for key, value := range vars {
+		args = append(args, "-v", fmt.Sprintf("%s=%s", key, value))
+	}
+
+	return exec.Command("bosh", args...).Run()
+}
+
+func (BOSHCLI) UploadRelease(address, caCertPath, username, password, releasePath string) error {
+	err := exec.Command("bosh",
+		"--ca-cert", caCertPath,
+		"--client", username,
+		"--client-secret", password,
+		"-e", address,
+		"upload-release", releasePath,
+	).Run()
+	if err != nil {
+		fmt.Printf("bosh upload-release output: %s\n", string(err.(*exec.ExitError).Stderr))
+	}
+	return err
+}
+
+func (BOSHCLI) UploadStemcell(address, caCertPath, username, password, stemcellPath string) error {
+	return exec.Command("bosh",
+		"--ca-cert", caCertPath,
+		"--client", username,
+		"--client-secret", password,
+		"-e", address,
+		"upload-stemcell", stemcellPath,
+	).Run()
+}
+
+func (BOSHCLI) VMs(address, caCertPath, username, password, deployment string) (string, error) {
+	output, err := exec.Command("bosh",
+		"--ca-cert", caCertPath,
+		"--client", username,
+		"--client-secret", password,
+		"-e", address,
+		"-d", deployment,
+		"vms",
+	).Output()
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
+}
+
+func (BOSHCLI) DeleteDeployment(address, caCertPath, username, password, deployment string) error {
+	return exec.Command("bosh",
+		"--ca-cert", caCertPath,
+		"--client", username,
+		"--client-secret", password,
+		"-e", address,
+		"-d", deployment,
+		"delete-deployment",
+		"-n",
+	).Run()
+}
--- acceptance-tests/concourse/deploy_test.go
+++ acceptance-tests/concourse/deploy_test.go
@@ -5,20 +5,17 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
-	"os/exec"
+	"strings"
 	"time"
 
 	acceptance "github.com/cloudfoundry/bosh-bootloader/acceptance-tests"
+	"github.com/cloudfoundry/bosh-bootloader/testhelpers"
 
 	"github.com/cloudfoundry/bosh-bootloader/acceptance-tests/actors"
-	"github.com/cloudfoundry/bosh-bootloader/testhelpers"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
-
-	"github.com/pivotal-cf-experimental/bosh-test/bosh"
 )
 
 var _ = Describe("concourse deployment test", func() {
@@ -27,8 +24,12 @@ var _ = Describe("concourse deployment test", func() {
 		state         acceptance.State
 		lbURL         string
 		configuration acceptance.Config
-		boshClient    bosh.Client
+		boshCLI       actors.BOSHCLI
 		sshSession    *gexec.Session
+		username      string
+		password      string
+		address       string
+		caCertPath    string
 	)
 
 	BeforeEach(func() {
@@ -51,23 +52,20 @@ var _ = Describe("concourse deployment test", func() {
 		session = bbl.CreateLB("concourse", certPath, keyPath, "")
 		Eventually(session, 10*time.Minute).Should(gexec.Exit(0))
 
-		session = bbl.UpdateLB(certPath, keyPath, "")
-		Eventually(session, 10*time.Minute).Should(gexec.Exit(0))
-
 		lbURL, err = actors.LBURL(configuration, bbl, state)
 		Expect(err).NotTo(HaveOccurred())
 
-		boshClient = bosh.NewClient(bosh.Config{
-			URL:              bbl.DirectorAddress(),
-			Username:         bbl.DirectorUsername(),
-			Password:         bbl.DirectorPassword(),
-			AllowInsecureSSL: true,
-		})
+		boshCLI = actors.NewBOSHCLI()
+
+		username = bbl.DirectorUsername()
+		password = bbl.DirectorPassword()
+		address = bbl.DirectorAddress()
+		caCertPath = bbl.DirectorCACert()
 	})
 
 	AfterEach(func() {
 		if sshSession != nil {
-			boshClient.DeleteDeployment("concourse")
+			boshCLI.DeleteDeployment(address, caCertPath, username, password, "concourse")
 			sshSession.Interrupt()
 			Eventually(sshSession, "5s").Should(gexec.Exit())
 		}
@@ -81,55 +79,33 @@ var _ = Describe("concourse deployment test", func() {
 		})
 
 		By("uploading releases and stemcells", func() {
-			err := uploadRelease(boshClient, configuration.ConcourseReleasePath)
+			err := boshCLI.UploadRelease(address, caCertPath, username, password, configuration.ConcourseReleasePath)
 			Expect(err).NotTo(HaveOccurred())
 
-			err = uploadRelease(boshClient, configuration.GardenReleasePath)
+			err = boshCLI.UploadRelease(address, caCertPath, username, password, configuration.GardenReleasePath)
 			Expect(err).NotTo(HaveOccurred())
 
-			err = uploadStemcell(boshClient, configuration.StemcellPath)
+			err = boshCLI.UploadStemcell(address, caCertPath, username, password, configuration.StemcellPath)
 			Expect(err).NotTo(HaveOccurred())
 		})
 
 		By("running bosh deploy and checking all the vms are running", func() {
-			os.Setenv("BOSH_CLIENT", bbl.DirectorUsername())
-			os.Setenv("BOSH_CLIENT_SECRET", bbl.DirectorPassword())
-			os.Setenv("BOSH_ENVIRONMENT", bbl.DirectorAddress())
-			os.Setenv("BOSH_CA_CERT", bbl.DirectorCACert())
-			args := []string{
-				"-d", "concourse",
-				"deploy",
+			err := boshCLI.Deploy(address, caCertPath, username, password, "concourse",
 				fmt.Sprintf("%s/concourse-deployment.yml", configuration.ConcourseDeploymentPath),
-				"-o", fmt.Sprintf("%s/operations/%s.yml", configuration.ConcourseDeploymentPath, configuration.IAAS),
-				"--vars-store", "concourse-vars.yml",
-				"-v", fmt.Sprintf("domain=%s", lbURL),
-				"-n",
-			}
-			cmd := exec.Command("bosh", args...)
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			err := cmd.Run()
+				"concourse-vars.yml",
+				[]string{fmt.Sprintf("%s/operations/%s.yml", configuration.ConcourseDeploymentPath, configuration.IAAS)},
+				map[string]string{"domain": lbURL},
+			)
 			Expect(err).NotTo(HaveOccurred())
 
-			Eventually(func() ([]bosh.VM, error) {
-				vms, err := boshClient.DeploymentVMs("concourse")
+			Eventually(func() int {
+				vms, err := boshCLI.VMs(address, caCertPath, username, password, "concourse")
 				if err != nil {
-					return []bosh.VM{}, err
+					return 0
 				}
 
-				vmsNoID := []bosh.VM{}
-				for _, vm := range vms {
-					vm.ID = ""
-					vm.IPs = nil
-					vmsNoID = append(vmsNoID, vm)
-				}
-				return vmsNoID, nil
-			}, "1m", "10s").Should(ConsistOf([]bosh.VM{
-				{JobName: "worker", Index: 0, State: "running"},
-				{JobName: "worker", Index: 1, State: "running"},
-				{JobName: "db", Index: 0, State: "running"},
-				{JobName: "web", Index: 0, State: "running"},
-			}))
+				return strings.Count(vms, "running")
+			}, "1m", "10s").Should(Equal(4))
 		})
 
 		By("testing the deployment", func() {
@@ -150,7 +126,7 @@ var _ = Describe("concourse deployment test", func() {
 		})
 
 		By("deleting the deployment", func() {
-			err := boshClient.DeleteDeployment("concourse")
+			err := boshCLI.DeleteDeployment(address, caCertPath, username, password, "concourse")
 			Expect(err).NotTo(HaveOccurred())
 		})
 
@@ -160,37 +136,3 @@ var _ = Describe("concourse deployment test", func() {
 		})
 	})
 })
-
-func uploadStemcell(boshClient bosh.Client, stemcellPath string) error {
-	stemcell, err := openFile(stemcellPath)
-	if err != nil {
-		return err
-	}
-
-	_, err = boshClient.UploadStemcell(stemcell)
-	return err
-}
-
-func uploadRelease(boshClient bosh.Client, releasePath string) error {
-	release, err := openFile(releasePath)
-	if err != nil {
-		return err
-	}
-
-	_, err = boshClient.UploadRelease(release)
-	return err
-}
-
-func openFile(filePath string) (bosh.SizeReader, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
-	}
-
-	stat, err := file.Stat()
-	if err != nil {
-		return nil, err
-	}
-
-	return bosh.NewSizeReader(file, stat.Size()), nil
-}
