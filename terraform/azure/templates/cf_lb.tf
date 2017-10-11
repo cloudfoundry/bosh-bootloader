fb13bb377ea81984dc886a1447a390989f5c923d
Nicolas Roy <niroy@microsoft.com>
2017-10-10 18:20:59 -0700

    Fix azure app gateway terraform

--- terraform/azure/templates/cf_lb.tf
+++ terraform/azure/templates/cf_lb.tf
@@ -1,28 +1,28 @@
-# Create a application gateway in the web_servers resource group
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = "my-vnet-12345"
+# # Create a application gateway in the web_servers resource group
+# resource "azurerm_virtual_network" "bosh" {
+#   name                = "${var.env_id}-cf-vnet"
 #   resource_group_name = "${azurerm_resource_group.bosh.name}"
 #   address_space       = ["10.254.0.0/16"]
-#   location            = "${azurerm_resource_group.rg.location}"
+#   location            = "${var.location}"
 # }
 
 resource "azurerm_subnet" "sub1" {
-  name                 = "subnet-1"
+  name                 = "${var.env_id}-cf-subnet1"
   resource_group_name  = "${azurerm_resource_group.bosh.name}"
   virtual_network_name = "${azurerm_virtual_network.bosh.name}"
   address_prefix       = "10.254.0.0/24"
 }
 
 resource "azurerm_subnet" "sub2" {
-  name                 = "subnet-2"
+  name                 = "${var.env_id}-cf-subnet2"
   resource_group_name  = "${azurerm_resource_group.bosh.name}"
   virtual_network_name = "${azurerm_virtual_network.bosh.name}"
   address_prefix       = "10.254.2.0/24"
 }
 
 resource "azurerm_public_ip" "lb" {
-  name                         = "${var.env_id}-lb"
-  location                     = "${azurerm_resource_group.rg.location}"
+  name                         = "${var.env_id}-cf-lb-ip"
+  location                     = "${var.location}"
   resource_group_name          = "${azurerm_resource_group.bosh.name}"
   public_ip_address_allocation = "dynamic"
 }
@@ -40,26 +40,26 @@ resource "azurerm_application_gateway" "network" {
   }
  
   gateway_ip_configuration {
-      name         = "{var.env_id}-gateway-ip-configuration"
-      subnet_id    = "${azurerm_virtual_network.vnet.id}/subnets/${azurerm_subnet.sub1.name}"
+      name         = "{var.env_id}-cf-gateway-ip-configuration"
+      subnet_id    = "${azurerm_virtual_network.bosh.id}/subnets/${azurerm_subnet.sub1.name}"
   }
  
   frontend_port {
-      name         = "{var.env_id}-frontend-port"
+      name         = "{var.env_id}-cf-frontend-port"
       port         = 80
   }
  
   frontend_ip_configuration {
-      name         = "{var.env_id}-frontend-ip-configuration"  
+      name         = "{var.env_id}-cf-frontend-ip-configuration"  
       public_ip_address_id = "${azurerm_public_ip.lb.id}"
   }
 
   backend_address_pool {
-      name = "my-backend-address-pool"
+      name = "${var.env_id}-cf-backend-address-pool"
   }
  
   backend_http_settings {
-      name                  = "${azurerm_virtual_network.vnet.name}-be-htst"
+      name                  = "${azurerm_virtual_network.bosh.name}-be-htst"
       cookie_based_affinity = "Disabled"
       port                  = 80
       protocol              = "Http"
@@ -67,17 +67,17 @@ resource "azurerm_application_gateway" "network" {
   }
  
   http_listener {
-        name                                  = "${azurerm_virtual_network.vnet.name}-httplstn"
+        name                                  = "${azurerm_virtual_network.bosh.name}-httplstn"
         frontend_ip_configuration_name        = "my-frontend-ip-configuration"
         frontend_port_name                    = "my-frontend-port"
         protocol                              = "Http"
   }
  
   request_routing_rule {
-          name                       = "${azurerm_virtual_network.vnet.name}-rqrt"
+          name                       = "${azurerm_virtual_network.bosh.name}-rqrt"
           rule_type                  = "Basic"
-          http_listener_name         = "${azurerm_virtual_network.vnet.name}-httplstn"
+          http_listener_name         = "${azurerm_virtual_network.bosh.name}-httplstn"
           backend_address_pool_name  = "my-backend-address-pool"
-          backend_http_settings_name = "${azurerm_virtual_network.vnet.name}-be-htst"
+          backend_http_settings_name = "${azurerm_virtual_network.bosh.name}-be-htst"
   }
 }
