135a86e0c7cdbbce9cf8104b438da1d7338ede3a
Lance French <lfrench@pivotal.io>
2017-10-12 10:23:14 -0700

    Removes redundant option flag

--- commands/print_env.go
+++ commands/print_env.go
@@ -84,7 +84,7 @@ func (p PrintEnv) Execute(args []string, state storage.State) error {
 
 	p.logger.Println(fmt.Sprintf("export BOSH_ALL_PROXY=socks5://localhost:%s", portNumber))
 	p.logger.Println(fmt.Sprintf("export JUMPBOX_PRIVATE_KEY=%s", privateKeyPath))
-	p.logger.Println(fmt.Sprintf("ssh -f -N -o StrictHostKeyChecking=no -o -o ServerAliveInterval=300 -D %s jumpbox@%s -i $JUMPBOX_PRIVATE_KEY", portNumber, jumpboxURL))
+	p.logger.Println(fmt.Sprintf("ssh -f -N -o StrictHostKeyChecking=no -o ServerAliveInterval=300 -D %s jumpbox@%s -i $JUMPBOX_PRIVATE_KEY", portNumber, jumpboxURL))
 
 	return nil
 }
