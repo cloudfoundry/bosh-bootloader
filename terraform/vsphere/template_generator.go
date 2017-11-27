3d92612dd163025842ea0c8277ed9f1c0eafe98c
Dave McClure <dmcclure@pivotal.io>
2017-11-27 12:36:14 -0800

    Director and jumpbox IPs are configurable for vsphere.
    [#152908821]
    
    Signed-off-by: Genevieve LEsperance <glesperance@pivotal.io>

--- terraform/vsphere/template_generator.go
+++ terraform/vsphere/template_generator.go
@@ -15,16 +15,19 @@ func NewTemplateGenerator() TemplateGenerator {
 func (t TemplateGenerator) Generate(state storage.State) string {
 	return fmt.Sprintf(`
 variable "vsphere_subnet" {}
-variable "external_ip" {}
+variable "jumpbox_ip" {}
 variable "internal_gw" {}
 variable "network_name" {}
 variable "vcenter_cluster" {}
+variable "bosh_director_internal_ip" {}
 
 output "internal_cidr" { value = "${var.vsphere_subnet}" }
 output "internal_gw" { value = "${var.internal_gw}" }
 output "network_name" { value = "${var.network_name}" }
 output "vcenter_cluster" { value = "${var.vcenter_cluster}" }
-output "external_ip" { value = "${var.external_ip}" }
-output "jumpbox_url" { value = "${var.external_ip}:22" }
+output "jumpbox_url" { value = "${var.jumpbox_ip}:22" }
+output "external_ip" { value = "${var.jumpbox_ip}" }
+output "jumpbox_internal_ip" { value = "${var.jumpbox_ip}" }
+output "bosh_director_internal_ip" { value = "${var.bosh_director_internal_ip}" }
 `)
 }
