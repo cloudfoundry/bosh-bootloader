39e84e20a8828b373320793a57a07da71e4fc922
Nick Roy <niroy@microsoft.com>
2017-12-01 07:24:17 -0800

    Add input for cert and key
    Signed-off-by: Genevieve LEsperance <glesperance@pivotal.io>

--- terraform/azure/input_generator.go
+++ terraform/azure/input_generator.go
@@ -1,6 +1,8 @@
 package azure
 
 import (
+	"encoding/base64"
+	"io/ioutil"
 	"strings"
 
 	"github.com/cloudfoundry/bosh-bootloader/storage"
@@ -26,6 +28,22 @@ func (i InputGenerator) Generate(state storage.State) (map[string]interface{}, e
 		"tenant_id":       state.Azure.TenantID,
 		"client_id":       state.Azure.ClientID,
 		"client_secret":   state.Azure.ClientSecret,
+	}
+
+	if state.LB.Cert != "" && state.LB.Key != "" {
+
+		certBytes, err := ioutil.ReadFile(state.LB.Cert)
+		if err != nil {
+			return map[string]interface{}{}, err
+		}
+		certBase64 :=  base64.StdEncoding.EncodeToString(certBytes)
+		input["pfx_cert_base64"] = certBase64
+
+		keyBytes, err := ioutil.ReadFile(state.LB.Key)
+		if err != nil {
+			return map[string]interface{}{}, err
+		}
+		input["pfx_key"] = string(keyBytes)
 	}
 
 	return input, nil
--- terraform/azure/input_generator_test.go
+++ terraform/azure/input_generator_test.go
@@ -1,6 +1,8 @@
 package azure_test
 
 import (
+	"io/ioutil"
+	"os"
 	"github.com/cloudfoundry/bosh-bootloader/storage"
 	"github.com/cloudfoundry/bosh-bootloader/terraform/azure"
 	. "github.com/onsi/ginkgo"
@@ -60,6 +62,67 @@ var _ = Describe("InputGenerator", func() {
 				"client_id":       state.Azure.ClientID,
 				"client_secret":   state.Azure.ClientSecret,
 			}))
+		})
+	})
+
+	Context("given a partial LB state", func() {
+		It("does not generate input for the LB", func(){
+			state.LB.Cert = "certpath"
+			inputs, err := inputGenerator.Generate(state)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(inputs)).To(Equal(7))
+		})
+	})
+
+	Context("given a LB", func() {
+		var (
+			cert *os.File
+			key *os.File
+		)
+
+		BeforeEach(func() {
+			var err error
+
+			cert, err = ioutil.TempFile("", "")
+			Expect(err).NotTo(HaveOccurred())
+			cert.WriteString("cert content")
+
+			key, err = ioutil.TempFile("", "")
+			Expect(err).NotTo(HaveOccurred())
+			key.WriteString("secret key")
+
+			state.LB.Cert = cert.Name()
+			state.LB.Key = key.Name()
+		})
+
+		AfterEach(func() {
+			cert.Close()
+			key.Close()
+		})
+
+		It("returns the expected inputs for the LB", func() {
+			inputs, err := inputGenerator.Generate(state)
+			Expect(err).NotTo(HaveOccurred())
+
+			inputs["pfx_cert_base64"] = ""
+
+			Expect(inputs).To(Equal(map[string]interface{}{
+				"simple_env_id":   "envid",
+				"env_id":          state.EnvID,
+				"region":        state.Azure.Region,
+				"subscription_id": state.Azure.SubscriptionID,
+				"tenant_id":       state.Azure.TenantID,
+				"client_id":       state.Azure.ClientID,
+				"client_secret":   state.Azure.ClientSecret,
+				"pfx_cert_base64": "",
+				"pfx_key":         "secret key",
+			}))
+		})
+
+		It("converts the cert to base64", func() {
+			inputs, err := inputGenerator.Generate(state)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(inputs["pfx_cert_base64"]).To(Equal("Y2VydCBjb250ZW50"))
 		})
 	})
 })
