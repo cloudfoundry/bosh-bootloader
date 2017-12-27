766536197ba06b9d1c845ae9dc3cfb62cac4ed9a
Genevieve LEsperance <glesperance@pivotal.io>
2017-12-26 17:22:44 -0800

    Add RDP port for jumpbox to internal vms on aws.
    [#153672955]

--- terraform/aws/fixtures/template_cf_lb_with_domain.tf
+++ terraform/aws/fixtures/template_cf_lb_with_domain.tf
@@ -429,6 +429,15 @@ resource "aws_security_group_rule" "jumpbox_ssh" {
   cidr_blocks       = ["${var.bosh_inbound_cidr}"]
 }
 
+resource "aws_security_group_rule" "jumpbox_rdp" {
+  security_group_id = "${aws_security_group.jumpbox.id}"
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 3389
+  to_port           = 3389
+  cidr_blocks       = ["${var.bosh_inbound_cidr}"]
+}
+
 resource "aws_security_group_rule" "jumpbox_agent" {
   security_group_id = "${aws_security_group.jumpbox.id}"
   type              = "ingress"
