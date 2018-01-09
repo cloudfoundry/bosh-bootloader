d404be6af023847677e367c58a3c924b166e67d2
Rowan Jacobs <rojacobs@pivotal.io>
2018-01-09 09:34:52 -0800

    Remove unused internal_gw from TF outputs
    - leaving it there causes panics when upgrading a vsphere env from
    v5.10.x to v5.11.x
    
    Signed-off-by: Desmond Pompa Alarcon Rawls <captaingrover@gmail.com>

--- bosh/manager.go
+++ bosh/manager.go
@@ -372,6 +372,7 @@ func (m *Manager) GetJumpboxDeploymentVars(state storage.State, terraformOutputs
 		allOutputs[k] = v
 	}
 	delete(allOutputs, "external_ip")
+	delete(allOutputs, "internal_gw")
 	allOutputs["internal_cidr"] = internalCIDR
 
 	vars := sharedDeploymentVarsYAML{
@@ -451,6 +452,7 @@ func (m *Manager) GetDirectorDeploymentVars(state storage.State, terraformOutput
 		allOutputs[k] = v
 	}
 	delete(allOutputs, "external_ip")
+	delete(allOutputs, "internal_gw")
 	allOutputs["internal_cidr"] = internalCIDR
 
 	vars := sharedDeploymentVarsYAML{
