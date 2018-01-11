7f361b9ecff15fb90a6332554853311e92edfdd2
Genevieve LEsperance <glesperance@pivotal.io>
2018-01-11 10:01:11 -0800

    Fix bbl-lite plan patch for terraform passthrough pr.
    [#153953834]

--- plan-patches/bosh-lite-gcp/terraform/bosh-lite_override.tf
+++ plan-patches/bosh-lite-gcp/terraform/bosh-lite_override.tf
@@ -1,11 +1,11 @@
 resource "google_compute_firewall" "bosh-director-lite" {
-  name = "${var.env_id}-bosh-director-lite"
+  name    = "${var.env_id}-bosh-director-lite"
   network = "${google_compute_network.bbl-network.name}"
 
   source_ranges = ["0.0.0.0/0"]
 
   allow {
-    ports = ["80", "443", "2222"]
+    ports    = ["80", "443", "2222"]
     protocol = "tcp"
   }
 
@@ -23,9 +23,9 @@ resource "google_compute_route" "bosh-lite-vms" {
   next_hop_ip = "10.0.0.6"
   priority    = 1
 
-  depends_on  = ["google_compute_subnetwork.bbl-subnet"]
+  depends_on = ["google_compute_subnetwork.bbl-subnet"]
 }
 
-output "director_external_ip" {
-    value = "${google_compute_address.bosh-director-ip.address}"
+output "director__external_ip" {
+  value = "${google_compute_address.bosh-director-ip.address}"
 }
