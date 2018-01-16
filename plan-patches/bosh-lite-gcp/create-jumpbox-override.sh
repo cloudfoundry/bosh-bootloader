afbc964e0aa4db353bddfd5310248bf0a0b2734a
Genevieve LEsperance <glesperance@pivotal.io>
2018-01-16 07:55:46 -0800

    Pass IAAS creds in create/delete bbl-lite scripts.
    - Since they are no longer written to the vars files.
    
    [#153914625]

--- plan-patches/bosh-lite-gcp/create-jumpbox-override.sh
+++ plan-patches/bosh-lite-gcp/create-jumpbox-override.sh
@@ -4,5 +4,8 @@ bosh create-env \
   --state  ${BBL_STATE_DIR}/vars/jumpbox-state.json \
   --vars-store  ${BBL_STATE_DIR}/vars/jumpbox-vars-store.yml \
   --vars-file  ${BBL_STATE_DIR}/vars/jumpbox-vars-file.yml \
+  --var-file gcp_credentials_json="${BBL_GCP_SERVICE_ACCOUNT_KEY_PATH}" \
+  -v project_id="${BBL_GCP_PROJECT_ID}" \
+  -v zone="${BBL_GCP_ZONE}" \
   -o  ${BBL_STATE_DIR}/jumpbox-deployment/gcp/cpi.yml \
   -o ${BBL_STATE_DIR}/ip-forwarding.yml
--- plan-patches/bosh-lite-gcp/delete-director-override.sh
+++ plan-patches/bosh-lite-gcp/delete-director-override.sh
@@ -4,6 +4,9 @@ bosh delete-env \
   --state  ${BBL_STATE_DIR}/vars/bosh-state.json \
   --vars-store  ${BBL_STATE_DIR}/vars/director-vars-store.yml \
   --vars-file  ${BBL_STATE_DIR}/vars/director-vars-file.yml \
+  --var-file gcp_credentials_json="${BBL_GCP_SERVICE_ACCOUNT_KEY_PATH}" \
+  -v project_id="${BBL_GCP_PROJECT_ID}" \
+  -v zone="${BBL_GCP_ZONE}" \
   -o  ${BBL_STATE_DIR}/bosh-deployment/gcp/cpi.yml \
   -o  ${BBL_STATE_DIR}/bosh-deployment/jumpbox-user.yml \
   -o  ${BBL_STATE_DIR}/bosh-deployment/uaa.yml \
