4e12ebe41106540de0443fbb02dfe4401dceceea
Genevieve LEsperance <glesperance@pivotal.io>
2018-01-22 13:49:48 -0800

    Add probes and rules for azure concourse lb.

--- terraform/azure/templates/concourse_lb.tf
+++ terraform/azure/templates/concourse_lb.tf
@@ -1,18 +1,13 @@
-resource "azurerm_lb_rule" "concourse-https" {
-  resource_group_name            = "${azurerm_resource_group.bosh.name}"
-  loadbalancer_id                = "${azurerm_lb.concourse.id}"
-  name                           = "${var.env_id}-cconcourse"
-  protocol                       = "Tcp"
-  frontend_port                  = 443
-  backend_port                   = 443
-  frontend_ip_configuration_name = "${var.env_id}-concourse-frontend-ip-configuration"
-}
-
 resource "azurerm_public_ip" "concourse" {
   name                         = "${var.env_id}-concourse-lb"
   location                     = "${var.region}"
   resource_group_name          = "${azurerm_resource_group.bosh.name}"
   public_ip_address_allocation = "static"
+  domain_name_label            = "${var.env_id}"
+
+  tags {
+    environment = "${var.env_id}"
+  }
 }
 
 resource "azurerm_lb" "concourse" {
@@ -24,6 +19,72 @@ resource "azurerm_lb" "concourse" {
     name                 = "${var.env_id}-concourse-frontend-ip-configuration"
     public_ip_address_id = "${azurerm_public_ip.concourse.id}"
   }
+}
+
+resource "azurerm_lb_rule" "concourse-ssh" {
+  name                = "${var.env_id}-concourse-ssh"
+  resource_group_name = "${azurerm_resource_group.bosh.name}"
+  loadbalancer_id     = "${azurerm_lb.concourse.id}"
+
+  frontend_ip_configuration_name = "${var.env_id}-concourse-frontend-ip-configuration"
+  protocol                       = "TCP"
+  frontend_port                  = 2222
+  backend_port                   = 2222
+
+  backend_address_pool_id = "${azurerm_lb_backend_address_pool.concourse.id}"
+  probe_id                = "${azurerm_lb_probe.concourse-ssh.id}"
+}
+
+resource "azurerm_lb_probe" "concourse-ssh" {
+  name                = "${var.env_id}-concourse-ssh"
+  resource_group_name = "${azurerm_resource_group.bosh.name}"
+  loadbalancer_id     = "${azurerm_lb.concourse.id}"
+  protocol            = "TCP"
+  port                = 2222
+}
+
+resource "azurerm_lb_rule" "concourse-https" {
+  name                = "${var.env_id}-concourse-https"
+  resource_group_name = "${azurerm_resource_group.bosh.name}"
+  loadbalancer_id     = "${azurerm_lb.concourse.id}"
+
+  frontend_ip_configuration_name = "${var.env_id}-concourse-frontend-ip-configuration"
+  protocol                       = "TCP"
+  frontend_port                  = 443
+  backend_port                   = 443
+
+  backend_address_pool_id = "${azurerm_lb_backend_address_pool.concourse.id}"
+  probe_id                = "${azurerm_lb_probe.concourse-https.id}"
+}
+
+resource "azurerm_lb_probe" "concourse-https" {
+  name                = "${var.env_id}-concourse-https"
+  resource_group_name = "${azurerm_resource_group.bosh.name}"
+  loadbalancer_id     = "${azurerm_lb.concourse.id}"
+  protocol            = "TCP"
+  port                = 443
+}
+
+resource "azurerm_lb_rule" "concourse-http" {
+  name                = "${var.env_id}-concourse-http"
+  resource_group_name = "${azurerm_resource_group.bosh.name}"
+  loadbalancer_id     = "${azurerm_lb.concourse.id}"
+
+  frontend_ip_configuration_name = "${var.env_id}-concourse-frontend-ip-configuration"
+  protocol                       = "TCP"
+  frontend_port                  = 80
+  backend_port                   = 80
+
+  backend_address_pool_id = "${azurerm_lb_backend_address_pool.concourse.id}"
+  probe_id                = "${azurerm_lb_probe.concourse-http.id}"
+}
+
+resource "azurerm_lb_probe" "concourse-http" {
+  name                = "${var.env_id}-concourse-http"
+  resource_group_name = "${azurerm_resource_group.bosh.name}"
+  loadbalancer_id     = "${azurerm_lb.concourse.id}"
+  protocol            = "TCP"
+  port                = 80
 }
 
 resource "azurerm_lb_backend_address_pool" "concourse" {
