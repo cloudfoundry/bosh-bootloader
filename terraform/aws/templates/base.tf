d8af173679a44b784993f3abc7680bc9ff6b04bd
Desmond Pompa Alarcon Rawls <captaingrover@gmail.com>
2018-02-13 10:35:55 -0800

    Close credhub port 8844 on the jumpbox on aws.
    Signed-off-by: Genevieve LEsperance <glesperance@pivotal.io>

--- terraform/aws/templates/base.tf
+++ terraform/aws/templates/base.tf
@@ -337,15 +337,6 @@ resource "aws_security_group_rule" "jumpbox_agent" {
   cidr_blocks       = ["${var.bosh_inbound_cidr}"]
 }
 
-resource "aws_security_group_rule" "jumpbox_credhub" {
-  security_group_id = "${aws_security_group.jumpbox.id}"
-  type              = "ingress"
-  protocol          = "tcp"
-  from_port         = 8844
-  to_port           = 8844
-  cidr_blocks       = ["${var.bosh_inbound_cidr}"]
-}
-
 resource "aws_security_group_rule" "jumpbox_director" {
   security_group_id = "${aws_security_group.jumpbox.id}"
   type              = "ingress"
