af066b03eb987b0535961ab70958e30b91e64847
Genevieve LEsperance <glesperance@pivotal.io>
2018-02-28 11:47:03 -0800

    Add openstack creds to help.

--- commands/commands_usage_test.go
+++ commands/commands_usage_test.go
@@ -21,28 +21,42 @@ var _ = Describe("Commands Usage", func() {
   --iaas                     IAAS to deploy your BOSH director onto: "aws", "azure", "gcp", "vsphere"   env: $BBL_IAAS
   --name                     Name to assign to your BOSH director (optional)                            env: $BBL_ENV_NAME
 
-  --aws-access-key-id        AWS Access Key ID              env: $BBL_AWS_ACCESS_KEY_ID
-  --aws-secret-access-key    AWS Secret Access Key          env: $BBL_AWS_SECRET_ACCESS_KEY
-  --aws-region               AWS Region                     env: $BBL_AWS_REGION
+  --aws-access-key-id                AWS Access Key ID                env: $BBL_AWS_ACCESS_KEY_ID
+  --aws-secret-access-key            AWS Secret Access Key            env: $BBL_AWS_SECRET_ACCESS_KEY
+  --aws-region                       AWS Region                       env: $BBL_AWS_REGION
 
-  --gcp-service-account-key  GCP Service Access Key to use  env: $BBL_GCP_SERVICE_ACCOUNT_KEY
-  --gcp-region               GCP Region to use              env: $BBL_GCP_REGION
+  --gcp-service-account-key          GCP Service Access Key to use    env: $BBL_GCP_SERVICE_ACCOUNT_KEY
+  --gcp-region                       GCP Region to use                env: $BBL_GCP_REGION
 
-  --azure-subscription-id    Azure Subscription ID          env: $BBL_AZURE_SUBSCRIPTION_ID
-  --azure-tenant-id          Azure Tenant ID                env: $BBL_AZURE_TENANT_ID
-  --azure-client-id          Azure Client ID                env: $BBL_AZURE_CLIENT_ID
-  --azure-client-secret      Azure Client Secret            env: $BBL_AZURE_CLIENT_SECRET
-  --azure-region             Azure Region                   env: $BBL_AZURE_REGION
+  --azure-subscription-id            Azure Subscription ID            env: $BBL_AZURE_SUBSCRIPTION_ID
+  --azure-tenant-id                  Azure Tenant ID                  env: $BBL_AZURE_TENANT_ID
+  --azure-client-id                  Azure Client ID                  env: $BBL_AZURE_CLIENT_ID
+  --azure-client-secret              Azure Client Secret              env: $BBL_AZURE_CLIENT_SECRET
+  --azure-region                     Azure Region                     env: $BBL_AZURE_REGION
 
-  --vsphere-vcenter-user     vSphere vCenter User           env: $BBL_VSPHERE_VCENTER_USER
-  --vsphere-vcenter-password vSphere vCenter Password       env: $BBL_VSPHERE_VCENTER_PASSWORD
-  --vsphere-vcenter-ip       vSphere vCenter IP             env: $BBL_VSPHERE_VCENTER_IP
-  --vsphere-vcenter-dc       vSphere vCenter Datacenter     env: $BBL_VSPHERE_VCENTER_DC
-  --vsphere-vcenter-cluster  vSphere vCenter Cluster        env: $BBL_VSPHERE_VCENTER_CLUSTER
-  --vsphere-vcenter-rp       vSphere vCenter Resource Pool  env: $BBL_VSPHERE_VCENTER_RP
-  --vsphere-network          vSphere Network                env: $BBL_VSPHERE_NETWORK
-  --vsphere-vcenter-ds       vSphere vCenter Datastore      env: $BBL_VSPHERE_VCENTER_DS
-  --vsphere-subnet           vSphere Subnet                 env: $BBL_VSPHERE_SUBNET
+  --vsphere-vcenter-user             vSphere vCenter User             env: $BBL_VSPHERE_VCENTER_USER
+  --vsphere-vcenter-password         vSphere vCenter Password         env: $BBL_VSPHERE_VCENTER_PASSWORD
+  --vsphere-vcenter-ip               vSphere vCenter IP               env: $BBL_VSPHERE_VCENTER_IP
+  --vsphere-vcenter-dc               vSphere vCenter Datacenter       env: $BBL_VSPHERE_VCENTER_DC
+  --vsphere-vcenter-cluster          vSphere vCenter Cluster          env: $BBL_VSPHERE_VCENTER_CLUSTER
+  --vsphere-vcenter-rp               vSphere vCenter Resource Pool    env: $BBL_VSPHERE_VCENTER_RP
+  --vsphere-network                  vSphere Network                  env: $BBL_VSPHERE_NETWORK
+  --vsphere-vcenter-ds               vSphere vCenter Datastore        env: $BBL_VSPHERE_VCENTER_DS
+  --vsphere-subnet                   vSphere Subnet                   env: $BBL_VSPHERE_SUBNET
+
+  --openstack-internal-cidr          OpenStack Internal CIDR          env: $BBL_OPENSTACK_INTERNAL_CIDR
+  --openstack-external-ip            OpenStack External IP            env: $BBL_OPENSTACK_EXTERNAL_IP
+  --openstack-auth-url               OpenStack Auth URL               env: $BBL_OPENSTACK_AUTH_URL
+  --openstack-az                     OpenStack Availability Zone      env: $BBL_OPENSTACK_AZ
+  --openstack-default-key-name       OpenStack Default Key Name       env: $BBL_OPENSTACK_DEFAULT_KEY_NAME
+  --openstack-default-security-group OpenStack Default Security Group env: $BBL_OPENSTACK_DEFAULT_SECURITY_GROUP
+  --openstack-network-id             OpenStack Network ID             env: $BBL_OPENSTACK_NETWORK_ID
+  --openstack-password               OpenStack Password               env: $BBL_OPENSTACK_PASSWORD
+  --openstack-username               OpenStack Username               env: $BBL_OPENSTACK_USERNAME
+  --openstack-project                OpenStack Project                env: $BBL_OPENSTACK_PROJECT
+  --openstack-domain                 OpenStack Domain                 env: $BBL_OPENSTACK_DOMAIN
+  --openstack-region                 OpenStack Region                 env: $BBL_OPENSTACK_REGION
+  --openstack-private-key            OpenStack Private Key            env: $BBL_OPENSTACK_PRIVATE_KEY
 
   Load Balancer options:
   --lb-type                  Load balancer(s) type: "concourse" or "cf"
