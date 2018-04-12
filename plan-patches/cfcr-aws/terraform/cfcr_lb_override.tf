0f14010ef98568f0bda3f0a0f7aaaa30a7740d19
Connor Braa <connor.braa@gmail.com>
2018-04-12 10:08:22 -0700

    Use short env id for aws elb for cfcr patch.
    [#156703189]
    
    Signed-off-by: Genevieve LEsperance <glesperance@pivotal.io>

--- plan-patches/cfcr-aws/terraform/cfcr_lb_override.tf
+++ plan-patches/cfcr-aws/terraform/cfcr_lb_override.tf
@@ -4,28 +4,28 @@ resource "random_id" "kubernetes-cluster-tag" {
 
 resource "aws_subnet" "bosh_subnet" {
   tags {
-    Name = "${var.env_id}-bosh-subnet"
+    Name              = "${var.env_id}-bosh-subnet"
     KubernetesCluster = "${random_id.kubernetes-cluster-tag.b64}"
   }
 }
 
 resource "aws_security_group" "api" {
-    name        = "${var.env_id}-api-access"
-    vpc_id = "${local.vpc_id}"
+  name   = "${var.env_id}-api-access"
+  vpc_id = "${local.vpc_id}"
 
-    ingress {
-      from_port   = "8443"
-      to_port     = "8443"
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = "8443"
+    to_port     = "8443"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-      from_port       = 0
-      to_port         = 0
-      protocol        = "-1"
-      cidr_blocks     = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group_rule" "cfcr_api_to_internal" {
@@ -38,22 +38,22 @@ resource "aws_security_group_rule" "cfcr_api_to_internal" {
 }
 
 resource "aws_elb" "api" {
-    name               = "${var.env_id}-cfcr-api"
-    subnets = ["${aws_subnet.bosh_subnet.id}"]
-    security_groups = ["${aws_security_group.api.id}"]
+  name            = "${var.short_env_id}-cfcr-api"
+  subnets         = ["${aws_subnet.bosh_subnet.id}"]
+  security_groups = ["${aws_security_group.api.id}"]
 
-    listener {
-      instance_port      = "8443"
-      instance_protocol  = "tcp"
-      lb_port            = "8443"
-      lb_protocol        = "tcp"
-    }
+  listener {
+    instance_port     = "8443"
+    instance_protocol = "tcp"
+    lb_port           = "8443"
+    lb_protocol       = "tcp"
+  }
 
-    health_check {
-      healthy_threshold   = 2
-      unhealthy_threshold = 2
-      timeout             = 2
-      target              = "TCP:8443"
-      interval            = 5
-    }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 2
+    target              = "TCP:8443"
+    interval            = 5
+  }
 }
