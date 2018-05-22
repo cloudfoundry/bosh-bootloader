57bde2b9e1a8b41d1c000d9f3382d0f7570da579
Nick Mahoney <nmahoney@pivotal.io>
2018-05-22 12:10:59 -0700

    Remove old parts of deployment vars YAML struct
    [#156672427]
    
    Signed-off-by: Rowan Jacobs <rojacobs@pivotal.io>

--- bosh/deployment_vars_yaml.go
+++ bosh/deployment_vars_yaml.go
@@ -1,32 +1,5 @@
 package bosh
 
 type sharedDeploymentVarsYAML struct {
-	AWSYAML          AWSYAML                `yaml:",inline"`
-	GCPYAML          GCPYAML                `yaml:",inline"`
-	AzureYAML        AzureYAML              `yaml:",inline"`
-	VSphereYAML      VSphereYAML            `yaml:",inline"`
 	TerraformOutputs map[string]interface{} `yaml:",inline"`
-}
-
-type AWSYAML struct {
-	AccessKeyID     string `yaml:"access_key_id,omitempty"`
-	SecretAccessKey string `yaml:"secret_access_key,omitempty"`
-}
-
-type GCPYAML struct {
-	Zone           string `yaml:"zone,omitempty"`
-	ProjectID      string `yaml:"project_id,omitempty"`
-	CredentialJSON string `yaml:"gcp_credentials_json,omitempty"`
-}
-
-type AzureYAML struct {
-	SubscriptionID string `yaml:"subscription_id,omitempty"`
-	TenantID       string `yaml:"tenant_id,omitempty"`
-	ClientID       string `yaml:"client_id,omitempty"`
-	ClientSecret   string `yaml:"client_secret,omitempty"`
-}
-
-type VSphereYAML struct {
-	VCenterUser     string `yaml:"vcenter_user,omitempty"`
-	VCenterPassword string `yaml:"vcenter_password,omitempty"`
 }
