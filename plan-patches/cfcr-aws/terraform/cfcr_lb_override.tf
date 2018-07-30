909c4d7b96b60b0835a02574a95329a757f3a470
Connor Braa <connor.braa@gmail.com>
2018-07-30 15:16:14 -0700

    fix cfcr-aws lb subnet provisioning
    [#158414161]
    
    Signed-off-by: Christopher Hunter <hunter.christopher@icloud.com>

--- plan-patches/cfcr-aws/terraform/cfcr_lb_override.tf
+++ plan-patches/cfcr-aws/terraform/cfcr_lb_override.tf
@@ -9,6 +9,45 @@ resource "aws_subnet" "bosh_subnet" {
   }
 }
 
+resource "aws_subnet" "internal_subnets" {
+  tags {
+    Name              = "${var.env_id}-internal-subnet${count.index}"
+    KubernetesCluster = "${random_id.kubernetes_cluster_tag.b64}"
+  }
+}
+
+resource "aws_subnet" "k8s_lb_subnets" {
+  count             = "${length(var.availability_zones)}"
+  vpc_id            = "${local.vpc_id}"
+  cidr_block        = "${cidrsubnet(var.vpc_cidr, 8, count.index+5)}"
+  availability_zone = "${element(var.availability_zones, count.index)}"
+
+  tags {
+    Name              = "${var.env_id}-lb-subnet${count.index}"
+    KubernetesCluster = "${random_id.kubernetes_cluster_tag.b64}"
+  }
+
+  lifecycle {
+    ignore_changes = ["cidr_block", "availability_zone"]
+  }
+}
+
+resource "aws_route_table" "lb_route_table" {
+  vpc_id = "${local.vpc_id}"
+}
+
+resource "aws_route" "lb_route_table" {
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.ig.id}"
+  route_table_id         = "${aws_route_table.lb_route_table.id}"
+}
+
+resource "aws_route_table_association" "route_k8s_lb_subnets" {
+  count          = "${length(var.availability_zones)}"
+  subnet_id      = "${element(aws_subnet.k8s_lb_subnets.*.id, count.index)}"
+  route_table_id = "${aws_route_table.lb_route_table.id}"
+}
+
 resource "aws_security_group" "cfcr_api" {
   name   = "${var.short_env_id}-cfcr-api-access"
   vpc_id = "${local.vpc_id}"
@@ -39,7 +78,7 @@ resource "aws_security_group_rule" "cfcr_api_to_internal" {
 
 resource "aws_elb" "cfcr_api" {
   name            = "${var.short_env_id}-cfcr-api"
-  subnets         = ["${aws_subnet.bosh_subnet.id}"]
+  subnets         = ["${aws_subnet.k8s_lb_subnets.*.id}"]
   security_groups = ["${aws_security_group.cfcr_api.id}"]
 
   listener {
