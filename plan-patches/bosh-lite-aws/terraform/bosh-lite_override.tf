1c31d835f8543497422b22f2dee32c511b48e813
Paul Warren <paul.warren@emc.com>
2018-08-02 15:31:25 -0700

    Add plan-patch for aws
    Signed-off-by: Maria Shaldibina <mshaldibina@pivotal.io>
    Signed-off-by: Dave Walter <dwalter@pivotal.io>

---
+++ plan-patches/bosh-lite-aws/terraform/bosh-lite_override.tf
@@ -0,0 +1,27 @@
+
+resource "aws_security_group_rule" "bosh_security_group_rule_http" {
+  security_group_id        = "${aws_security_group.bosh_security_group.id}"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 80
+  to_port                  = 80
+  cidr_blocks = ["${var.bosh_inbound_cidr}"]
+}
+
+resource "aws_security_group_rule" "bosh_security_group_rule_https" {
+  security_group_id        = "${aws_security_group.bosh_security_group.id}"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 443
+  to_port                  = 443
+  cidr_blocks = ["${var.bosh_inbound_cidr}"]
+}
+
+resource "aws_security_group_rule" "bosh_security_group_rule_https_alt" {
+  security_group_id        = "${aws_security_group.bosh_security_group.id}"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 4443
+  to_port                  = 4443
+  cidr_blocks = ["${var.bosh_inbound_cidr}"]
+}
