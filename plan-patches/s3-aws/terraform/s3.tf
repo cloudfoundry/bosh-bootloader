171621702b019bdeb3a40095e928e4d8cb67fed5
arthomps <arthomps+github_spam@gmail.com>
2018-08-15 07:46:11 -0400

    Update s3.tf
    Updated to include:
    - encrypted s3
    - replica buckets in another zone
    -

--- plan-patches/s3-aws/terraform/s3.tf
+++ plan-patches/s3-aws/terraform/s3.tf
@@ -17,7 +17,192 @@ provider "aws" {
   region     = "${var.s3_replication_region}"
 }
 
-resource "aws_iam_role" "replication" {
+resource "aws_iam_user" "bbl_s3_user" {
+  count = "${length(var.s3_users)}"
+  name  = "${element(var.s3_users, count.index)}"
+}
+
+resource "aws_kms_key" "kms_key_s3_rep" {
+  provider            = "aws.s3_replication_provider"
+  enable_key_rotation = true
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "key-consolepolicy-3",
+  "Statement": [
+    {
+      "Sid": "Enable IAM User Permissions",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::481316288090:root"
+      },
+      "Action": "kms:*",
+      "Resource": "*"
+    },
+    {
+      "Sid": "Allow access for Key Administrators",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": ${jsonencode(aws_iam_user.bbl_s3_user.*.arn)}
+
+      },
+      "Action": [
+        "kms:Create*",
+        "kms:Describe*",
+        "kms:Enable*",
+        "kms:List*",
+        "kms:Put*",
+        "kms:Update*",
+        "kms:Revoke*",
+        "kms:Disable*",
+        "kms:Get*",
+        "kms:Delete*",
+        "kms:ScheduleKeyDeletion",
+        "kms:CancelKeyDeletion"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "Allow use of the key",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": ${jsonencode(concat(formatlist("%s",aws_iam_user.bbl_s3_user.*.arn),formatlist("%s",aws_iam_role.s3-replication.*.arn)))}
+      },
+      "Action": [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey",
+        "kms:Create*",
+        "kms:Describe*",
+        "kms:Enable*",
+        "kms:List*",
+        "kms:Put*",
+        "kms:Update*",
+        "kms:Revoke*",
+        "kms:Disable*",
+        "kms:Get*",
+        "kms:Delete*",
+        "kms:ScheduleKeyDeletion",
+        "kms:CancelKeyDeletion"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "Allow attachment of persistent resources",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": ${jsonencode(aws_iam_user.bbl_s3_user.*.arn)}
+      },
+      "Action": [
+        "kms:CreateGrant",
+        "kms:ListGrants",
+        "kms:RevokeGrant"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "Bool": {
+          "kms:GrantIsForAWSResource": "true"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_kms_key" "kms_key_s3" {
+  enable_key_rotation = true
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "key-consolepolicy-2",
+  "Statement": [
+    {
+      "Sid": "Enable IAM User Permissions",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::481316288090:root"
+      },
+      "Action": "kms:*",
+      "Resource": "*"
+    },
+    {
+      "Sid": "Allow access for Key Administrators",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": ${jsonencode(concat(formatlist("%s",aws_iam_user.bbl_s3_user.*.arn),formatlist("%s",aws_iam_role.s3-replication.*.arn)))}
+
+      },
+      "Action": [
+        "kms:Create*",
+        "kms:Describe*",
+        "kms:Enable*",
+        "kms:List*",
+        "kms:Put*",
+        "kms:Update*",
+        "kms:Revoke*",
+        "kms:Disable*",
+        "kms:Get*",
+        "kms:Delete*",
+        "kms:ScheduleKeyDeletion",
+        "kms:CancelKeyDeletion"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "Allow use of the key",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": ${jsonencode(concat(formatlist("%s",aws_iam_user.bbl_s3_user.*.arn),formatlist("%s",aws_iam_role.s3-replication.*.arn)))}
+      },
+      "Action": [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Sid": "Allow attachment of persistent resources",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": ${jsonencode(aws_iam_user.bbl_s3_user.*.arn)}
+      },
+      "Action": [
+        "kms:CreateGrant",
+        "kms:ListGrants",
+        "kms:RevokeGrant"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "Bool": {
+          "kms:GrantIsForAWSResource": "true"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_s3_bucket" "destinations" {
+  count      = "${length(var.s3_bucket_names)}"
+  provider   = "aws.s3_replication_provider"
+  bucket     = "${element(var.s3_bucket_names, count.index)}-replica"
+  region     = "${var.s3_replication_region}"
+
+
+  versioning {
+    enabled = true
+  }
+}
+resource "aws_iam_role" "s3-replication" {
   name = "${var.env_id}-s3-replication-role"
 
   assume_role_policy = <<POLICY
@@ -32,19 +217,15 @@ resource "aws_iam_role" "replication" {
       "Effect": "Allow",
       "Sid": ""
     }
+
   ]
 }
 POLICY
 }
 
-resource "aws_iam_user" "bbl_user" {
-  count = "${length(var.s3_users)}"
-  name  = "${element(var.s3_users, count.index)}"
-}
-
-resource "aws_iam_policy" "replication" {
+resource "aws_iam_policy" "s3-replication" {
   name       = "${var.env_id}-s3-replication-policy"
-  depends_on = ["aws_s3_bucket.zbucket"]
+  depends_on = ["aws_s3_bucket.zbuckets"]
 
   policy = <<POLICY
 {
@@ -52,23 +233,31 @@ resource "aws_iam_policy" "replication" {
   "Statement": [
     {
       "Action": [
+      "s3:GetObjectVersionForReplication",
+      "s3:GetObjectVersionAcl",
+        "s3:GetObjectVersionForReplication",
+        "s3:ListBucket",
         "s3:GetReplicationConfiguration",
-        "s3:ListBucket"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-      "${aws_s3_bucket.zbucket.0.arn}", "${aws_s3_bucket.zbucket.1.arn}","${aws_s3_bucket.zbucket.2.arn}","${aws_s3_bucket.zbucket.3.arn}"
-      ]
-    },
-    {
-      "Action": [
-        "s3:GetObjectVersion",
+        "s3:GetObjectVersionForReplication",
         "s3:GetObjectVersionAcl"
       ],
       "Effect": "Allow",
-      "Resource": [
-      "${aws_s3_bucket.zbucket.0.arn}/*", "${aws_s3_bucket.zbucket.1.arn}/*","${aws_s3_bucket.zbucket.2.arn}/*","${aws_s3_bucket.zbucket.3.arn}/*"
-      ]
+      "Resource":
+      ${jsonencode(aws_s3_bucket.zbuckets.*.arn)}
+    },
+    {
+      "Action": [
+      "s3:GetObjectVersionForReplication",
+      "s3:GetObjectVersionAcl",
+        "s3:GetObjectVersion",
+        "s3:ListBucket",
+        "s3:GetReplicationConfiguration",
+        "s3:GetObjectVersionForReplication",
+        "s3:GetObjectVersionAcl"
+      ],
+      "Effect": "Allow",
+      "Resource":
+      ${jsonencode(concat(formatlist("%s/*",aws_s3_bucket.zbuckets.*.arn)))}
     },
     {
       "Action": [
@@ -76,9 +265,70 @@ resource "aws_iam_policy" "replication" {
         "s3:ReplicateDelete"
       ],
       "Effect": "Allow",
-      "Resource": [
-      "${aws_s3_bucket.destination.0.arn}/*", "${aws_s3_bucket.destination.1.arn}/*", "${aws_s3_bucket.destination.2.arn}/*","${aws_s3_bucket.destination.3.arn}/*"
-     ]
+      "Resource":
+      ${jsonencode(concat(formatlist("%s/*",aws_s3_bucket.destinations.*.arn)))}
+    },
+    {
+    "Action": [
+            "kms:*"
+         ],
+    "Effect": "Allow",
+    "Resource": "*"
+    },
+    {
+        "Action": [
+            "s3:ReplicateObject",
+            "s3:ReplicateDelete",
+            "s3:ReplicateTags",
+            "s3:GetObjectVersionTagging"
+        ],
+        "Effect": "Allow",
+        "Condition": {
+            "StringLikeIfExists": {
+                "s3:x-amz-server-side-encryption": [
+                    "aws:kms",
+                    "AES256"
+                ],
+                "s3:x-amz-server-side-encryption-aws-kms-key-id": [
+                    "${aws_kms_key.kms_key_s3_rep.arn}"
+                ]
+            }
+        },
+        "Resource": ${jsonencode(concat(formatlist("%s/*",aws_s3_bucket.destinations.*.arn)))}
+    },
+    {
+        "Action": [
+            "kms:Decrypt"
+        ],
+        "Effect": "Allow",
+        "Condition": {
+            "StringLike": {
+                "kms:ViaService": "s3.us-east-1.amazonaws.com",
+                "kms:EncryptionContext:aws:s3:arn":
+                    ${jsonencode(concat(formatlist("%s/*",aws_s3_bucket.zbuckets.*.arn)))}
+
+            }
+        },
+        "Resource": [
+            "${aws_kms_key.kms_key_s3.arn}"
+        ]
+    },
+    {
+        "Action": [
+            "kms:Encrypt"
+        ],
+        "Effect": "Allow",
+        "Condition": {
+            "StringLike": {
+                "kms:ViaService": "s3.us-west-1.amazonaws.com",
+                "kms:EncryptionContext:aws:s3:arn":
+                    ${jsonencode(concat(formatlist("%s/*",aws_s3_bucket.destinations.*.arn)))}
+
+            }
+        },
+        "Resource": [
+            "${aws_kms_key.kms_key_s3_rep.arn}"
+        ]
     }
   ]
 }
@@ -88,7 +338,7 @@ POLICY
 resource "aws_s3_bucket_policy" "bucketpol" {
   count      = "${length(var.s3_bucket_names)}"
   bucket     = "${element(var.s3_bucket_names, count.index)}"
-  depends_on = ["aws_s3_bucket.zbucket"]
+  depends_on = ["aws_s3_bucket.zbuckets"]
 
   policy = <<POLICY
 {
@@ -99,7 +349,7 @@ resource "aws_s3_bucket_policy" "bucketpol" {
       "Sid": "statementi_${element(var.s3_bucket_names, count.index)}",
       "Effect": "Allow",
       "Principal": {
-      "AWS": ${jsonencode(aws_iam_user.bbl_user.*.arn)}
+      "AWS": ${jsonencode(aws_iam_user.bbl_s3_user.*.arn)}
        },
       "Action": "s3:*",
       "Resource": "arn:aws:s3:::${element(var.s3_bucket_names, count.index)}"
@@ -109,45 +359,112 @@ resource "aws_s3_bucket_policy" "bucketpol" {
 POLICY
 }
 
-resource "aws_iam_policy_attachment" "replication" {
+resource "aws_s3_bucket_policy" "bucketpol-replica" {
+  count      = "${length(var.s3_bucket_names)}"
+  bucket     = "${element(var.s3_bucket_names, count.index)}-replica"
+  provider   = "aws.s3_replication_provider"
+  depends_on = ["aws_s3_bucket.zbuckets"]
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "MYBUCKETPOLICY_{element(var.s3_bucket_names, count.index)}",
+  "Statement": [
+    {
+      "Sid": "statementi_${element(var.s3_bucket_names, count.index)}",
+      "Effect": "Allow",
+      "Principal": {
+      "AWS": ${jsonencode(aws_iam_user.bbl_s3_user.*.arn)}
+       },
+      "Action": "s3:*",
+      "Resource": "arn:aws:s3:::${element(var.s3_bucket_names, count.index)}-replica"
+     }
+  ]
+}
+POLICY
+}
+
+
+
+resource "aws_iam_policy_attachment" "s3-replication" {
   name       = "${var.env_id}-s3-replication-policy-attachment"
-  roles      = ["${aws_iam_role.replication.name}"]
-  policy_arn = "${aws_iam_policy.replication.arn}"
+  roles      = ["${aws_iam_role.s3-replication.name}"]
+  policy_arn = "${aws_iam_policy.s3-replication.arn}"
 }
-
-resource "aws_s3_bucket" "destination" {
-  count    = "${length(var.s3_bucket_names)}"
-  provider = "aws.s3_replication_provider"
-  bucket   = "${element(var.s3_bucket_names, count.index)}-replica"
-  region   = "${var.s3_replication_region}"
-
-  versioning {
-    enabled = true
-  }
-}
-
-resource "aws_s3_bucket" "zbucket" {
+resource "aws_s3_bucket" "zbuckets" {
   count      = "${length(var.s3_bucket_names)}"
   bucket     = "${element(var.s3_bucket_names, count.index)}"
   acl        = "private"
-  depends_on = ["aws_s3_bucket.destination"]
+  depends_on = ["aws_s3_bucket.destinations"]
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.kms_key_s3.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 
   versioning {
     enabled = true
   }
 
   replication_configuration {
-    role = "${aws_iam_role.replication.arn}"
+    role = "${aws_iam_role.s3-replication.arn}"
 
     rules {
       id     = "foobar"
       status = "Enabled"
       prefix = ""
 
+      source_selection_criteria {
+        sse_kms_encrypted_objects {
+          enabled = "true"
+        }
+      }
+
       destination {
-        bucket        = "arn:aws:s3:::${element(var.s3_bucket_names, count.index)}-replica"
-        storage_class = "STANDARD"
+        bucket             = "arn:aws:s3:::${element(var.s3_bucket_names, count.index)}-replica"
+        storage_class      = "STANDARD"
+        replica_kms_key_id = "${aws_kms_key.kms_key_s3_rep.arn}"
       }
     }
   }
+}
+
+resource "aws_iam_user_policy" "bbl_s3_user_policy" {
+  count      = "${length(var.s3_users)}"
+  name       = "${element(var.s3_users, count.index)}"
+  user       = "${element(var.s3_users, count.index)}"
+  depends_on = ["aws_s3_bucket.zbuckets"]
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource":
+            ${jsonencode(concat(formatlist("%s/*",aws_s3_bucket.zbuckets.*.arn),formatlist("%s/*",aws_s3_bucket.destinations.*.arn)))}
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListAllMyBuckets",
+                "s3:ListBucket",
+                "s3:HeadBucket"
+            ],
+            "Resource": "*"
+
+        },
+        {
+           "Effect": "Allow",
+           "Action": "iam:ListRoles",
+           "Resource": "*"
+        }
+    ]
+}
+EOF
 }
