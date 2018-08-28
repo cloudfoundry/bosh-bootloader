641c6b4ee20f941e74262231bea39dc4e1efc495
Oleksandr Slynko <oslynko@pivotal.io>
2018-08-28 14:08:46 +0100

    Only master VM requires password now
    [#158293331]

--- plan-patches/cfcr-vsphere/terraform/outputs-override.tf
+++ plan-patches/cfcr-vsphere/terraform/outputs-override.tf
@@ -6,10 +6,3 @@ output "vcenter_master_password" {
   value = "${var.vcenter_password}"
 }
 
-output "vcenter_worker_user" {
-  value = "${var.vcenter_user}"
-}
-
-output "vcenter_worker_password" {
-  value = "${var.vcenter_password}"
-}
