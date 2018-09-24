515e94bd4bd972ad271910717761863f774c49f5
Zac Bergquist <zbergquist99@gmail.com>
2018-09-24 14:16:20 -0600

    aws: include credhub/UAA target groups in outputs

--- terraform/aws/templates/concourse_lb.tf
+++ terraform/aws/templates/concourse_lb.tf
@@ -180,7 +180,13 @@ output "concourse_lb_internal_security_group" {
 }
 
 output "concourse_lb_target_groups" {
-  value = ["${aws_lb_target_group.concourse_lb_80.name}", "${aws_lb_target_group.concourse_lb_443.name}", "${aws_lb_target_group.concourse_lb_2222.name}"]
+  value = [
+    "${aws_lb_target_group.concourse_lb_80.name}",
+    "${aws_lb_target_group.concourse_lb_443.name}",
+    "${aws_lb_target_group.concourse_lb_2222.name}",
+    "${aws_lb_target_group.concourse_lb_8443.name}",
+    "${aws_lb_target_group.concourse_lb_8844.name}"
+  ]
 }
 
 output "concourse_lb_name" {
