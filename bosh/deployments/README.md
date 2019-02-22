6c2878dbc4d97a3512af4093b3e37fdb6811b499
Zach Gershman <zgershman@pivotal.io>
2019-02-22 10:46:27 -0800

    this converts all functionality over to packr2
    - there is no longer a usage of go-bindata
    - we had to do some interesting things related
    to file mod time
    - we've intentionally left a panic or two in
    the code.
    
    - adr to follow

---
+++ bosh/deployments/README.md
@@ -0,0 +1,12 @@
+## Why?
+
+This directory is now here to help with the usage of [packr2](https://godoc.org/github.com/gobuffalo/packr/v2)
+
+When building the actual bbl binary, this is where your jumpbox /
+bosh deployment repos should end up so they can be included in the
+final build.
+
+## What happens if this directory isn't empty
+
+Whatever jumpbox / bosh deployment repos you have residing in this
+directory will be built into the bbl. Use caution.
--- terraform/binary_path_test.go
+++ terraform/binary_path_test.go
@@ -7,7 +7,8 @@ import (
 
 	"github.com/cloudfoundry/bosh-bootloader/fakes"
 	"github.com/cloudfoundry/bosh-bootloader/terraform"
-	"github.com/cloudfoundry/bosh-bootloader/terraform/binary_dist"
+	"github.com/gobuffalo/packr/v2"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -15,23 +16,90 @@ import (
 var _ = Describe("BinaryPath", func() {
 	var (
 		fileSystem *fakes.FileIO
+
+		binary *terraform.Binary
 	)
 
 	BeforeEach(func() {
+		path := "/some/tmp/path"
 		fileSystem = &fakes.FileIO{}
-		fileSystem.GetTempDirCall.Returns.Name = "/some/tmp/path/"
+		fileSystem.GetTempDirCall.Returns.Name = path
 		fileSystem.ExistsCall.Returns.Bool = false
+		box := packr.New("terraform", "./binary_dist")
+
+		err := box.AddString("terraform", "my-terraform-binary")
+		Expect(err).NotTo(HaveOccurred())
+
+		err = box.AddString("terraform-mod-time", "1550769688")
+		Expect(err).NotTo(HaveOccurred())
+
+		binary = &terraform.Binary{
+			Path: path + "/bbl-terraform",
+			Box:  box,
+			FS:   fileSystem,
+		}
 	})
 
 	It("installs the binary from binary-dist and preserves modified timestamps", func() {
-		res, err := terraform.BinaryPathInjected(fileSystem)
+		res, err := binary.BinaryPath()
 		Expect(err).NotTo(HaveOccurred())
 		Expect(res).To(Equal("/some/tmp/path/bbl-terraform"))
 		Expect(fileSystem.WriteFileCall.Receives[0].Filename).To(Equal("/some/tmp/path/bbl-terraform"))
 		Expect(fileSystem.WriteFileCall.Receives[0].Mode).To(Equal(os.ModePerm))
+
+		modTime, err := binary.RetrieveModTime()
+		Expect(err).NotTo(HaveOccurred())
 		// this covers a critical corner case where the user installs
 		// an old version AFTER we updated the tf binary that we distribute.
-		Expect(fileSystem.ChtimesCall.Receives.ModTime).To(Equal(binary_dist.MustAssetInfo("terraform").ModTime()))
+		Expect(fileSystem.ChtimesCall.Receives.ModTime).To(Equal(modTime))
+	})
+
+	Context("when there is no my-terraform-binary in box", func() {
+		BeforeEach(func() {
+			box := packr.New("a-totally-different-box", "./binary_dist")
+
+			binary.Box = box
+		})
+
+		It("returns an error", func() {
+			_, err := binary.BinaryPath()
+			Expect(err).To(MatchError(ContainSubstring("no such file or directory")))
+		})
+	})
+
+	Context("when there is no terraform-mod-time in box", func() {
+		BeforeEach(func() {
+			box := packr.New("different-box", "./binary_dist")
+
+			err := box.AddString("terraform", "my-terraform-binary")
+			Expect(err).NotTo(HaveOccurred())
+
+			binary.Box = box
+		})
+
+		It("returns an error", func() {
+			_, err := binary.BinaryPath()
+			Expect(err).To(MatchError("could not find terraform-mod-time file"))
+		})
+	})
+
+	Context("when there is no terraform-mod-time in box", func() {
+		BeforeEach(func() {
+			box := packr.New("so-many-different-boxes", "./binary_dist")
+
+			err := box.AddString("terraform", "my-terraform-binary")
+			Expect(err).NotTo(HaveOccurred())
+
+			err = box.AddString("terraform-mod-time", "15507 not a valid number 69688")
+			Expect(err).NotTo(HaveOccurred())
+
+			binary.Box = box
+		})
+
+		It("returns an error", func() {
+			_, err := binary.BinaryPath()
+			Expect(err).To(MatchError(ContainSubstring("incorrect format of time in terraform-mod-time")))
+		})
 	})
 
 	Context("when there's an error trying to find the cached bbl binary", func() {
@@ -40,7 +108,7 @@ var _ = Describe("BinaryPath", func() {
 		})
 
 		It("errors", func() {
-			_, err := terraform.BinaryPathInjected(fileSystem)
+			_, err := binary.BinaryPath()
 			Expect(err).To(MatchError(fileSystem.ExistsCall.Returns.Error))
 		})
 	})
@@ -57,24 +125,23 @@ var _ = Describe("BinaryPath", func() {
 			})
 
 			It("errors", func() {
-				_, err := terraform.BinaryPathInjected(fileSystem)
+				_, err := binary.BinaryPath()
 				Expect(err).To(MatchError(fileSystem.StatCall.Returns.Error))
 			})
 		})
 
 		Context("that has the same modified timestamp as our binary dist", func() {
 			BeforeEach(func() {
-				info, err := binary_dist.AssetInfo("terraform")
+				modTime, err := binary.RetrieveModTime()
 				Expect(err).NotTo(HaveOccurred())
-				modtime := info.ModTime()
 
 				fileSystem.StatCall.Returns.FileInfo = fakes.FileInfo{
-					Modtime: &modtime,
+					Modtime: &modTime,
 				}
 			})
 
 			It("doesn't rewrite the file", func() {
-				res, err := terraform.BinaryPathInjected(fileSystem)
+				res, err := binary.BinaryPath()
 				Expect(err).NotTo(HaveOccurred())
 				Expect(res).To(Equal("/some/tmp/path/bbl-terraform"))
 				Expect(fileSystem.WriteFileCall.CallCount).To(Equal(0))
@@ -90,7 +157,7 @@ var _ = Describe("BinaryPath", func() {
 			})
 
 			It("rewrites the file", func() {
-				res, err := terraform.BinaryPathInjected(fileSystem)
+				res, err := binary.BinaryPath()
 				Expect(err).NotTo(HaveOccurred())
 				Expect(res).To(Equal("/some/tmp/path/bbl-terraform"))
 				Expect(fileSystem.WriteFileCall.CallCount).To(BeNumerically(">=", 1))
@@ -104,7 +171,7 @@ var _ = Describe("BinaryPath", func() {
 				})
 
 				It("errors", func() {
-					_, err := terraform.BinaryPathInjected(fileSystem)
+					_, err := binary.BinaryPath()
 					Expect(err).To(MatchError(fileSystem.WriteFileCall.Returns[0].Error))
 				})
 			})
@@ -115,7 +182,7 @@ var _ = Describe("BinaryPath", func() {
 				})
 
 				It("errors", func() {
-					_, err := terraform.BinaryPathInjected(fileSystem)
+					_, err := binary.BinaryPath()
 					Expect(err).To(MatchError(fileSystem.ChtimesCall.Returns.Error))
 				})
 			})
