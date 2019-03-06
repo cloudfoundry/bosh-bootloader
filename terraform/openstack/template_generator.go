8da6125f9993e183447fb0e73d1b55228d2e50d0
Zach Gershman <zgershman@pivotal.io>
2019-03-06 11:42:15 -0800

    we reintroduced go-bindata with a PR we merged
    - openstack support had not been converted to the
    new way of doing things

--- terraform/openstack/template_generator.go
+++ terraform/openstack/template_generator.go
@@ -4,7 +4,10 @@ import (
 	"strings"
 
 	"github.com/cloudfoundry/bosh-bootloader/storage"
+	"github.com/gobuffalo/packr/v2"
 )
+
+const templatesPath = "./templates"
 
 type templates struct {
 	providerVars     string
@@ -14,25 +17,51 @@ type templates struct {
 	resources        string
 }
 
-type TemplateGenerator struct{}
+type TemplateGenerator struct {
+	box *packr.Box
+}
 
 func NewTemplateGenerator() TemplateGenerator {
-	return TemplateGenerator{}
+	return TemplateGenerator{
+		box: packr.New("openstack-templates", templatesPath),
+	}
 }
 
 func (t TemplateGenerator) Generate(state storage.State) string {
-	tmpls := readTemplates()
+	tmpls := t.readTemplates()
 	template := strings.Join([]string{tmpls.providerVars, tmpls.provider, tmpls.resourcesOutputs, tmpls.resourcesVars, tmpls.resources}, "\n")
 	return template
 }
 
-func readTemplates() templates {
-	tmpls := templates{}
-	tmpls.providerVars = string(MustAsset("templates/provider-vars.tf"))
-	tmpls.provider = string(MustAsset("templates/provider.tf"))
-	tmpls.resourcesOutputs = string(MustAsset("templates/resources-outputs.tf"))
-	tmpls.resourcesVars = string(MustAsset("templates/resources-vars.tf"))
-	tmpls.resources = string(MustAsset("templates/resources.tf"))
+func (t TemplateGenerator) readTemplates() templates {
+	listings := map[string]string{
+		"provider-vars.tf":     "",
+		"provider.tf":          "",
+		"resources-outputs.tf": "",
+		"resources-vars.tf":    "",
+		"resources.tf":         "",
+	}
 
-	return tmpls
+	var errors []error
+	for item := range listings {
+		content, err := t.box.FindString(item)
+		if err != nil {
+			errors = append(errors, err)
+			continue
+		}
+
+		listings[item] = content
+	}
+
+	if errors != nil {
+		panic(errors)
+	}
+
+	return templates{
+		providerVars:     listings["provider-vars.tf"],
+		provider:         listings["provider.tf"],
+		resourcesOutputs: listings["resources-outputs.tf"],
+		resourcesVars:    listings["resources-vars.tf"],
+		resources:        listings["resources.tf"],
+	}
 }
