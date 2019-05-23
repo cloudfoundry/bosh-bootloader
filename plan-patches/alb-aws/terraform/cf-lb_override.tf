91d15f0b53eceee2664a69ccd409e647d47aaac5
Nick Rohn <nrohn@pivotal.io>
2019-05-23 11:02:43 -0700

    Correct plan patch variable references
    Signed-off-by: Sara Montecino <smontecino@pivotal.io>

--- plan-patches/alb-aws/terraform/cf-lb_override.tf
+++ plan-patches/alb-aws/terraform/cf-lb_override.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "cf_router" {
   name               = "${var.short_env_id}-cf-router-lb"
   load_balancer_type = "application"
 
-  security_groups = ["${aws_security_group.cf_router.id}"]
+  security_groups = ["${aws_security_group.cf_router_lb_security_group.id}"]
   subnets         = ["${aws_subnet.lb_subnets.*.id}"]
 }
 
@@ -85,28 +85,28 @@ resource "aws_security_group" "cf_router_lb_internal_security_group" {
   vpc_id      = "${local.vpc_id}"
 
   ingress {
-    security_groups = ["${aws_security_group.cf_router.id}"]
+    security_groups = ["${aws_security_group.cf_router_lb_security_group.id}"]
     protocol        = "tcp"
     from_port       = 80
     to_port         = 80
   }
 
   ingress {
-    security_groups = ["${aws_security_group.cf_router.id}"]
+    security_groups = ["${aws_security_group.cf_router_lb_security_group.id}"]
     protocol        = "tcp"
     from_port       = 8080
     to_port         = 8080
   }
 
   ingress {
-    security_groups = ["${aws_security_group.cf_router.id}"]
+    security_groups = ["${aws_security_group.cf_router_lb_security_group.id}"]
     protocol        = "tcp"
     from_port       = 443
     to_port         = 443
   }
 
   ingress {
-    security_groups = ["${aws_security_group.cf_router.id}"]
+    security_groups = ["${aws_security_group.cf_router_lb_security_group.id}"]
     protocol        = "tcp"
     from_port       = 4443
     to_port         = 4443
