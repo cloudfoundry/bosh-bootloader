40bdaeeed7f71a4e44f66289d6b2aa60de20cab2
Tom Viehman <tviehman@pivotal.io>
2019-08-07 10:36:21 -0700

    Don't set BOSH_LOG_LEVEL to debug
    This change makes the `create-director-override.sh` script consistent with its GCP counterpart, and also prevents users of this ops-file from accidentally sharing secrets (debug log level will show vars-file/vars-store content, among other things.)

--- plan-patches/bosh-lite-aws/create-director-override.sh
+++ plan-patches/bosh-lite-aws/create-director-override.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-BOSH_LOG_LEVEL=debug bosh create-env \
+bosh create-env \
  ${BBL_STATE_DIR}/bosh-deployment/bosh.yml \
  --state=state.json \
  --vars-store  ${BBL_STATE_DIR}/vars/director-vars-store.yml \
