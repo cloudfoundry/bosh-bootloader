a79502da1792997c40d4601968b36ab2a288c0ca
wildloop <mariusz@olejnik.net>
2019-09-05 15:03:13 +0200

    Update ssh.go

--- commands/ssh.go
+++ commands/ssh.go
@@ -159,10 +159,10 @@ func (s SSH) Execute(args []string, state storage.State) error {
 	if !s.pathFinder.CommandExists("connect-proxy") {
 		ncHelpOutBytes, err := exec.Command("nc", "-h").Output()
 		outStr := string(ncHelpOutBytes)
-		if err == nil && strings.Contains(outStr,"-x addr[:port]") {
+		if err == nil && strings.Contains(outStr,"netcat-openbsd") {
 			proxyCommandPrefix = "nc -x"
 		} else {
-			return fmt.Errorf("neither `connect-proxy` nor openbsd-`nc` (with proxy support: `-x addr[:port]`) can be executed")
+			return fmt.Errorf("neither `connect-proxy` nor openbsd `nc` (with proxy support) can be executed")
 		}
 	}
 	
