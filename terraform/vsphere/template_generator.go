007bba356c40defe91de291fa69a7890a4281959
Andrei Krasnitski <a.krasnitski@outlook.com>
2020-06-15 11:21:51 +0200

    Update Vsphere templates to support Terraform 12

--- terraform/vsphere/template_generator.go
+++ terraform/vsphere/template_generator.go
@@ -31,21 +31,21 @@ variable "vcenter_templates" {}
 variable "vcenter_vms" {}
 variable "vcenter_disks" {}
 
-output "internal_cidr" { value = "${var.vsphere_subnet_cidr}" }
-output "internal_gw" { value = "${var.internal_gw}" }
-output "network_name" { value = "${var.network_name}" }
-output "vcenter_cluster" { value = "${var.vcenter_cluster}" }
+output "internal_cidr" { value = var.vsphere_subnet_cidr }
+output "internal_gw" { value = var.internal_gw }
+output "network_name" { value = var.network_name }
+output "vcenter_cluster" { value = var.vcenter_cluster }
 output "jumpbox_url" { value = "${var.jumpbox_ip}:22" }
-output "external_ip" { value = "${var.jumpbox_ip}" }
-output "jumpbox__internal_ip" { value = "${var.jumpbox_ip}" }
-output "director__internal_ip" { value = "${var.director_internal_ip}" }
+output "external_ip" { value = var.jumpbox_ip }
+output "jumpbox__internal_ip" { value = var.jumpbox_ip }
+output "director__internal_ip" { value = var.director_internal_ip }
 output "director_name" { value = "bosh-${var.env_id}" }
-output "vcenter_disks" { value = "${var.vcenter_disks}" }
-output "vcenter_vms" { value = "${var.vcenter_vms}" }
-output "vcenter_templates" { value = "${var.vcenter_templates}" }
-output "vcenter_ip" { value = "${var.vcenter_ip}" }
-output "vcenter_dc" { value = "${var.vcenter_dc}" }
-output "vcenter_rp" { value = "${var.vcenter_rp}" }
-output "vcenter_ds" { value = "${var.vcenter_ds}" }
+output "vcenter_disks" { value = var.vcenter_disks }
+output "vcenter_vms" { value = var.vcenter_vms }
+output "vcenter_templates" { value = var.vcenter_templates }
+output "vcenter_ip" { value = var.vcenter_ip }
+output "vcenter_dc" { value = var.vcenter_dc }
+output "vcenter_rp" { value = var.vcenter_rp }
+output "vcenter_ds" { value = var.vcenter_ds }
 `)
 }
