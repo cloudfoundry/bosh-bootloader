9c1a6d76ba70266f032230c3a54bb5f4a34ff952
Tencho Dimitrov <tencho.dimitrov@sap.com>
2023-05-10 12:53:55 +0300

    fix plan-patch templates related to Azure

--- plan-patches/cf-azure/terraform/cf_subnet.tf
+++ plan-patches/cf-azure/terraform/cf_subnet.tf
@@ -3,5 +3,5 @@ resource "azurerm_subnet" "cf-subnet" {
   name                 = "${var.env_id}-cf-sn"
   resource_group_name  = "${azurerm_resource_group.bosh.name}"
   virtual_network_name = "${azurerm_virtual_network.bosh.name}"
-  address_prefix       = "${cidrsubnet(var.network_cidr, 4, 1)}"
+  address_prefixes     = ["${cidrsubnet(var.network_cidr, 4, 1)}"]
 }
