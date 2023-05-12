695592351b8ab54b54cd6dd8df540c41d547d33c
Jochen Ehret <jochen.ehret@sap.com>
2023-05-12 07:52:40 +0200

    Migrate AWS S3 plan-patches to Terraform v1.4.6
    * remove "_override" suffix as there is nothing to override
    * mark secret access key as "sensitive"
    * replace deprecated "template_file" data source with a heredoc

---
+++ plan-patches/s3-blobstore-aws/terraform/s3_blobstore_iam.tf
@@ -0,0 +1,37 @@
+resource "aws_iam_user" "blobstore_access" {
+  name = "${var.env_id}-s3-blobstore-access"
+}
+
+resource "aws_iam_access_key" "blobstore_access" {
+  user = "${aws_iam_user.blobstore_access.name}"
+}
+
+resource "aws_iam_user_policy" "blobstore_access" {
+  name = "${var.env_id}-s3-blobstore-access"
+  user = "${aws_iam_user.blobstore_access.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.buildpacks_bucket.arn}",
+        "${aws_s3_bucket.buildpacks_bucket.arn}/*",
+        "${aws_s3_bucket.droplets_bucket.arn}",
+        "${aws_s3_bucket.droplets_bucket.arn}/*",
+        "${aws_s3_bucket.packages_bucket.arn}",
+        "${aws_s3_bucket.packages_bucket.arn}/*",
+        "${aws_s3_bucket.resources_bucket.arn}",
+        "${aws_s3_bucket.resources_bucket.arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+
+}
