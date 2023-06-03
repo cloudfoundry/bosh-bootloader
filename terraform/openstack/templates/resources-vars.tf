33a659d1c87e804b3cece14931f22994606a2629
kinjelom <mariusz@olejnik.net>
2023-06-03 10:23:49 +0200

    Update resources-vars.tf

--- terraform/openstack/templates/resources-vars.tf
+++ terraform/openstack/templates/resources-vars.tf
@@ -21,3 +21,23 @@ variable "ext_net_name" {
 variable "region_name" {
   description = "OpenStack region name"
 }
+
+variable "subnet_cidr" {
+  description = "CIDR representing IP range for this subnet, IPv4"
+  default = "10.0.1.0/24"
+}
+
+variable "subnet_allocation_pool_start" {
+  description = "The start IP address available for use with DHCP in this subnet, each IP range must be from the same CIDR that the subnet is part of"
+  default = "10.0.1.200"
+}
+
+variable "subnet_allocation_pool_end" {
+  description = "The end IP address available for use with DHCP in this subnet, each IP range must be from the same CIDR that the subnet is part of"
+  default = "10.0.1.254"
+}
+
+variable "subnet_gateway_ip" {
+  description = "Default gateway used by devices in this subnet"
+  default = "10.0.1.1"
+}
