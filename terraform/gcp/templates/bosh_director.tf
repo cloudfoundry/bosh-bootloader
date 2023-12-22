c0d89c8b58e0d81c19cba028de94958f43a8cd6c
Jochen Ehret <jochen.ehret@sap.com>
2023-12-22 10:51:22 +0100

    Enable dynamic port allocation for GCP Cloud NAT gateway
    * with static port allocation we have a performance degradation

--- terraform/gcp/templates/bosh_director.tf
+++ terraform/gcp/templates/bosh_director.tf
@@ -120,6 +120,7 @@ resource "google_compute_router_nat" "nat" {
   region                             = "${var.region}"
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  enable_dynamic_port_allocation     = true
 
   log_config {
     enable = true
