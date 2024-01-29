de13684679b9ae663eecfa6d360f4b7af85e8b55
Mario Di Miceli <mario.dimiceli@orange.com>
2024-01-29 11:08:09 +0100

    removing required providers for cloudstack

--- terraform/cloudstack/templates/provider.tf
+++ terraform/cloudstack/templates/provider.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    cloudstack = {
-      source = "orange-cloudfoundry/cloudstack"
-    }
-    local      = {
-      source = "hashicorp/local"
-    }
-    template   = {
-      source = "hashicorp/template"
-    }
-    tls        = {
-      source = "hashicorp/tls"
-    }
-  }
-  required_version = ">= 0.13"
-}
-
 provider "cloudstack" {
   api_url    = var.cloudstack_endpoint
   api_key    = var.cloudstack_api_key
