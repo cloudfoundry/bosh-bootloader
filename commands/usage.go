618bb14dba43e6bc494e05e452dcbb6e8eecfb46
Mario Di Miceli <mario.dimiceli@orange.com>
2024-05-14 09:44:40 +0200

    add global flag

--- commands/usage.go
+++ commands/usage.go
@@ -13,11 +13,12 @@ Usage:
   bbl [GLOBAL OPTIONS] %s [OPTIONS]
 
 Global Options:
-  --help       [-h]        Prints usage. Use "bbl [command] --help" for more information about a command
-  --state-dir  [-s]        Directory containing the bbl state                                            env:"BBL_STATE_DIRECTORY"
-  --debug      [-d]        Prints debugging output                                                       env:"BBL_DEBUG"
-  --version    [-v]        Prints version
-  --no-confirm [-n]        No confirm
+  --help                 [-h] Prints usage. Use "bbl [command] --help" for more information about a command
+  --state-dir            [-s] Directory containing the bbl state                                            env:"BBL_STATE_DIRECTORY"
+  --debug                [-d] Prints debugging output                                                       env:"BBL_DEBUG"
+  --version              [-v] Prints version
+  --no-confirm           [-n] No confirm
+  --use-tf-local-binary  [-u] Use the local terraform binary if it exists                                   env:"BBL_USE_TF_LOCAL_BINARY"
 %s
 `
 	CommandUsage = `
