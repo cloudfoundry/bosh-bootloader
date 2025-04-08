ac13203e51bafb17659e5b881745ec3549085fbd
Alexander Lais <alexander.lais@sap.com>
2025-04-08 23:13:55 +0200

    feat: add IPv6 to IP and CIDRBlock types
    Uses net/netip.Addr and .Prefix (CIDR) to ensure the same computation.
    
    Tests, similar to IPv4 tests were added.

--- bosh/cidr_block_test.go
+++ bosh/cidr_block_test.go
@@ -71,4 +71,64 @@ var _ = Describe("CIDRBlock", func() {
 			})
 		})
 	})
+	Context("v6", func() {
+		BeforeEach(func() {
+			var err error
+			cidrBlock, err = bosh.ParseCIDRBlock("2001:db8:cf::/80")
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		Describe("GetFirstIP", func() {
+			It("returns the first ip of the cidr block", func() {
+				ip := cidrBlock.GetFirstIP()
+				Expect(ip.String()).To(Equal("2001:db8:cf::"))
+			})
+		})
+
+		Describe("GetNthIP", func() {
+			It("returns the nth ip of the cidr block", func() {
+				ip := cidrBlock.GetNthIP(6)
+				Expect(ip.String()).To(Equal("2001:db8:cf::6"))
+			})
+		})
+
+		Describe("GetLastIP", func() {
+			It("returns the first ip of the cidr block", func() {
+				ip := cidrBlock.GetLastIP()
+				Expect(ip.String()).To(Equal("2001:db8:cf::ffff:ffff:ffff"))
+			})
+		})
+
+		Describe("ParseCIDRBlock", func() {
+			Context("failure cases", func() {
+				Context("when input string is not a valid CIDR block", func() {
+					It("returns an error", func() {
+						_, err := bosh.ParseCIDRBlock("whatever")
+						Expect(err).To(MatchError(ContainSubstring("no '/'")))
+					})
+				})
+
+				Context("when input string contains an invalid ip", func() {
+					It("returns an error", func() {
+						_, err := bosh.ParseCIDRBlock("not-an-ip/96")
+						Expect(err).To(MatchError(ContainSubstring("unable to parse IP")))
+					})
+				})
+
+				Context("when input string contains mask bits which are not an integer", func() {
+					It("returns an error", func() {
+						_, err := bosh.ParseCIDRBlock("2001:db8:cf::/not-mask-bits")
+						Expect(err).To(MatchError(ContainSubstring(`bad bits after slash: "not-mask-bits"`)))
+					})
+				})
+
+				Context("when input string contains mask bits which are out of range", func() {
+					It("returns an error", func() {
+						_, err := bosh.ParseCIDRBlock("2001:db8:cf::/243")
+						Expect(err).To(MatchError(ContainSubstring("prefix length out of range")))
+					})
+				})
+			})
+		})
+	})
 })
--- bosh/ip_test.go
+++ bosh/ip_test.go
@@ -9,66 +9,66 @@ import (
 
 var _ = Describe("IP", func() {
 	Context("v4", func() {
-	Describe("ParseIP", func() {
-		It("returns an IP object that represents IP from string", func() {
-			ip, err := bosh.ParseIP("10.0.16.255")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(ip.String()).To(Equal("10.0.16.255"))
+		Describe("ParseIP", func() {
+			It("returns an IP object that represents IP from string", func() {
+				ip, err := bosh.ParseIP("10.0.16.255")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(ip.String()).To(Equal("10.0.16.255"))
+			})
+
+			Context("failure cases", func() {
+				It("returns an error if it cannot parse ip", func() {
+					_, err := bosh.ParseIP("not valid")
+					Expect(err).To(MatchError(ContainSubstring("unable to parse IP")))
+				})
+
+				It("returns an error if ip parts are not digits", func() {
+					_, err := bosh.ParseIP("x.x.x.x")
+					Expect(err).To(MatchError(ContainSubstring("unexpected character")))
+				})
+
+				It("returns an error if ip parts are out of the allowed range", func() {
+					_, err := bosh.ParseIP("999.999.999.999")
+					Expect(err).To(MatchError(ContainSubstring("IPv4 field has value >255")))
+				})
+
+				It("returns an error if ip has too many parts", func() {
+					_, err := bosh.ParseIP("1.1.1.1.1.1.1")
+					Expect(err).To(MatchError(ContainSubstring("IPv4 address too long")))
+				})
+			})
 		})
 
-		Context("failure cases", func() {
-			It("returns an error if it cannot parse ip", func() {
-				_, err := bosh.ParseIP("not valid")
-				Expect(err).To(MatchError(ContainSubstring("unable to parse IP")))
+		Describe("Add", func() {
+			It("returns an IP object that represents IP offsetted by 1", func() {
+				ip, err := bosh.ParseIP("10.0.16.1")
+				ip = ip.Add(1)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(ip.String()).To(Equal("10.0.16.2"))
 			})
+		})
 
-			It("returns an error if ip parts are not digits", func() {
-				_, err := bosh.ParseIP("x.x.x.x")
-				Expect(err).To(MatchError(ContainSubstring("unexpected character")))
+		Describe("Subtract", func() {
+			It("returns an IP object that represents IP offsetted by -1", func() {
+				ip, err := bosh.ParseIP("10.0.16.2")
+				ip = ip.Subtract(1)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(ip.String()).To(Equal("10.0.16.1"))
 			})
+		})
 
-			It("returns an error if ip parts are out of the allowed range", func() {
-				_, err := bosh.ParseIP("999.999.999.999")
-				Expect(err).To(MatchError(ContainSubstring("IPv4 field has value >255")))
-			})
-
-			It("returns an error if ip has too many parts", func() {
-				_, err := bosh.ParseIP("1.1.1.1.1.1.1")
-				Expect(err).To(MatchError(ContainSubstring("IPv4 address too long")))
+		Describe("String", func() {
+			It("returns a string representation of IP object", func() {
+				ip, err := bosh.ParseIP("10.0.16.1")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(ip.String()).To(Equal("10.0.16.1"))
 			})
 		})
 	})
-
-	Describe("Add", func() {
-		It("returns an IP object that represents IP offsetted by 1", func() {
-			ip, err := bosh.ParseIP("10.0.16.1")
-			ip = ip.Add(1)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(ip.String()).To(Equal("10.0.16.2"))
-		})
-	})
-
-	Describe("Subtract", func() {
-		It("returns an IP object that represents IP offsetted by -1", func() {
-			ip, err := bosh.ParseIP("10.0.16.2")
-			ip = ip.Subtract(1)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(ip.String()).To(Equal("10.0.16.1"))
-		})
-	})
-
-	Describe("String", func() {
-		It("returns a string representation of IP object", func() {
-			ip, err := bosh.ParseIP("10.0.16.1")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(ip.String()).To(Equal("10.0.16.1"))
-		})
-	})
-})
 	Context("v6", func() {
 		Describe("ParseIP", func() {
 			It("returns an IP object that represents IP from string", func() {
-				ip, err := bosh.ParseIP("2001:db8:cf:0:0:ffff:1337")
+				ip, err := bosh.ParseIP("2001:db8:cf:0:0:0:ffff:1337")
 				Expect(err).NotTo(HaveOccurred())
 				Expect(ip.String()).To(Equal("2001:db8:cf::ffff:1337"))
 			})
@@ -76,22 +76,22 @@ var _ = Describe("IP", func() {
 			Context("failure cases", func() {
 				It("returns an error if it cannot parse ip", func() {
 					_, err := bosh.ParseIP("2001:db8:cf::not valid")
-					Expect(err).To(MatchError(ContainSubstring("unable to parse IP")))
+					Expect(err).To(MatchError(ContainSubstring("each colon-separated field must have at least one digit")))
 				})
 
 				It("returns an error if ip parts are not digits", func() {
 					_, err := bosh.ParseIP("2001:db8:cf:x:x:x:x")
-					Expect(err).To(MatchError(ContainSubstring("unexpected character")))
+					Expect(err).To(MatchError(ContainSubstring("each colon-separated field must have at least one digit")))
 				})
 
 				It("returns an error if ip parts are out of the allowed range", func() {
 					_, err := bosh.ParseIP("2001:db8:cf:G::")
-					Expect(err).To(MatchError(ContainSubstring("IPv6 field has value >F")))
+					Expect(err).To(MatchError(ContainSubstring("each colon-separated field must have at least one digit")))
 				})
 
 				It("returns an error if ip has too many parts", func() {
 					_, err := bosh.ParseIP("2001:db8:cf:0:0:0:ffff:ffff:ffff")
-					Expect(err).To(MatchError(ContainSubstring("IPv6 address too long")))
+					Expect(err).To(MatchError(ContainSubstring("trailing garbage after address")))
 				})
 			})
 		})
