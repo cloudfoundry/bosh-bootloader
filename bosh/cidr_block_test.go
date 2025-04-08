8c02aa9901ead8c0147b300d5373cb5998cb0b49
Alexander Lais <alexander.lais@sap.com>
2025-04-08 22:40:26 +0200

    feat: move IP and CIDRBlock to standard types
    Uses net/netip.Addr and .Prefix (CIDR) to ensure the same computation.
    
    Error messages are now different, so tests were adapted there. All other tests remain.

--- bosh/cidr_block_test.go
+++ bosh/cidr_block_test.go
@@ -11,61 +11,62 @@ var _ = Describe("CIDRBlock", func() {
 	var (
 		cidrBlock bosh.CIDRBlock
 	)
-
-	BeforeEach(func() {
-		var err error
-		cidrBlock, err = bosh.ParseCIDRBlock("10.0.16.0/20")
-		Expect(err).NotTo(HaveOccurred())
-	})
-
-	Describe("GetFirstIP", func() {
-		It("returns the first ip of the cidr block", func() {
-			ip := cidrBlock.GetFirstIP()
-			Expect(ip.String()).To(Equal("10.0.16.0"))
+	Context("v4", func() {
+		BeforeEach(func() {
+			var err error
+			cidrBlock, err = bosh.ParseCIDRBlock("10.0.16.0/20")
+			Expect(err).NotTo(HaveOccurred())
 		})
-	})
 
-	Describe("GetNthIP", func() {
-		It("returns the nth ip of the cidr block", func() {
-			ip := cidrBlock.GetNthIP(6)
-			Expect(ip.String()).To(Equal("10.0.16.6"))
+		Describe("GetFirstIP", func() {
+			It("returns the first ip of the cidr block", func() {
+				ip := cidrBlock.GetFirstIP()
+				Expect(ip.String()).To(Equal("10.0.16.0"))
+			})
 		})
-	})
 
-	Describe("GetLastIP", func() {
-		It("returns the first ip of the cidr block", func() {
-			ip := cidrBlock.GetLastIP()
-			Expect(ip.String()).To(Equal("10.0.31.255"))
+		Describe("GetNthIP", func() {
+			It("returns the nth ip of the cidr block", func() {
+				ip := cidrBlock.GetNthIP(6)
+				Expect(ip.String()).To(Equal("10.0.16.6"))
+			})
 		})
-	})
 
-	Describe("ParseCIDRBlock", func() {
-		Context("failure cases", func() {
-			Context("when input string is not a valid CIDR block", func() {
-				It("returns an error", func() {
-					_, err := bosh.ParseCIDRBlock("whatever")
-					Expect(err).To(MatchError(ContainSubstring("cannot parse CIDR block")))
-				})
+		Describe("GetLastIP", func() {
+			It("returns the first ip of the cidr block", func() {
+				ip := cidrBlock.GetLastIP()
+				Expect(ip.String()).To(Equal("10.0.31.255"))
 			})
+		})
 
-			Context("when input string contains an invalid ip", func() {
-				It("returns an error", func() {
-					_, err := bosh.ParseCIDRBlock("not-an-ip/20")
-					Expect(err).To(MatchError(ContainSubstring("not a valid ip address")))
+		Describe("ParseCIDRBlock", func() {
+			Context("failure cases", func() {
+				Context("when input string is not a valid CIDR block", func() {
+					It("returns an error", func() {
+						_, err := bosh.ParseCIDRBlock("whatever")
+						Expect(err).To(MatchError(ContainSubstring("no '/'")))
+					})
 				})
-			})
 
-			Context("when input string contains mask bits which are not an integer", func() {
-				It("returns an error", func() {
-					_, err := bosh.ParseCIDRBlock("0.0.0.0/not-mask-bits")
-					Expect(err).To(MatchError(ContainSubstring("invalid syntax")))
+				Context("when input string contains an invalid ip", func() {
+					It("returns an error", func() {
+						_, err := bosh.ParseCIDRBlock("not-an-ip/20")
+						Expect(err).To(MatchError(ContainSubstring("unable to parse IP")))
+					})
 				})
-			})
 
-			Context("when input string contains mask bits which are out of range", func() {
-				It("returns an error", func() {
-					_, err := bosh.ParseCIDRBlock("0.0.0.0/243")
-					Expect(err).To(MatchError(ContainSubstring("mask bits out of range")))
+				Context("when input string contains mask bits which are not an integer", func() {
+					It("returns an error", func() {
+						_, err := bosh.ParseCIDRBlock("0.0.0.0/not-mask-bits")
+						Expect(err).To(MatchError(ContainSubstring(`bad bits after slash: "not-mask-bits"`)))
+					})
+				})
+
+				Context("when input string contains mask bits which are out of range", func() {
+					It("returns an error", func() {
+						_, err := bosh.ParseCIDRBlock("0.0.0.0/243")
+						Expect(err).To(MatchError(ContainSubstring("prefix length out of range")))
+					})
 				})
 			})
 		})
