64f95e59724449893745494377d0c1a8b8da277f
ramonskie <ramonmakkelie@gmail.com>
2026-07-10 11:00:18 +0200

    add leftovers to on_failure. make leftovers a function and make it reuasable

--- ci/configure.sh
+++ ci/configure.sh
@@ -2,5 +2,10 @@
 
 set -eu
 
+pipeline="$(mktemp)"
+trap 'rm -f "${pipeline}"' EXIT
+
+ytt -f ci/pipelines/bosh-bootloader.yml --ignore-unknown-comments > "${pipeline}"
+
 fly -t bosh set-pipeline -p bosh-bootloader \
-    -c ci/pipelines/bosh-bootloader.yml
+    -c "${pipeline}"
